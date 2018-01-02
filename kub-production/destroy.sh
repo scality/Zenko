2237efc9204b0db3e4f8c12ee5b1330a4c3a74bd
Anurag Mittal <anurag.213@gmail.com>
2018-01-02 11:56:46 -0800

    added cache and moved lb to frontend-dmz

--- kub-production/destroy.sh
+++ kub-production/destroy.sh
@@ -2,10 +2,12 @@ kubectl delete service s3-data
 kubectl delete service s3-metadata
 kubectl delete service s3-front
 kubectl delete service zenko-prod-lb
+kubectl delete service cache
 kubectl delete deployment s3-data
 kubectl delete deployment s3-metadata
 kubectl delete deployment s3-front
 kubectl delete deployment zenko-prod-lb
+kubectl delete deployment cache
 kubectl delete pv s3-data-pv
 kubectl delete pv s3-metadata-pv
 kubectl delete pvc s3-data-pv-claim
