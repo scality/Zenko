17a8b7cf685d023e01d90a01c345c08f0e5515de
Nicolas Trangez <ikke@nicolast.be>
2018-04-13 10:17:21 +0200

    Tests: Add basic Prometheus tests

--- tests/zenko_e2e/cloudserver/test_basic.py
+++ tests/zenko_e2e/cloudserver/test_basic.py
@@ -1,26 +1,14 @@
-import os
-import boto3
-
 import pytest
-
-
-@pytest.fixture(scope='session')
-def client():
-    endpoint_url = os.environ['CLOUDSERVER_FRONT_ENDPOINT']
-    return boto3.client(
-        service_name='s3',
-        endpoint_url=endpoint_url,
-    )
 
 
 @pytest.mark.nondestructive
 @pytest.mark.conformance
-def test_list_buckets(client):
-    assert 'Buckets' in client.list_buckets()
+def test_list_buckets(zenko_s3_client):
+    assert 'Buckets' in zenko_s3_client.list_buckets()
 
 
 @pytest.mark.conformance
-def test_create_bucket(client):
-    response = client.create_bucket(Bucket='zenko-e2e')
+def test_create_bucket(zenko_s3_client):
+    response = zenko_s3_client.create_bucket(Bucket='zenko-e2e')
     assert 'Location' in response
-    client.delete_bucket(Bucket='zenko-e2e')
+    zenko_s3_client.delete_bucket(Bucket='zenko-e2e')
