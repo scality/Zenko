9152ee192bef1d5066c309315338477d823d44d8
Taylor McKinnon <mail@tmacs.space>
2018-07-19 13:23:17 -0700

    add retries to MPUs

--- tests/zenko_e2e/cloudserver/test_mpu.py
+++ tests/zenko_e2e/cloudserver/test_mpu.py
@@ -3,26 +3,31 @@ from ..fixtures import *
 from .. import util
 
 
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.conformance
 def test_mpu_aws(aws_ep_bucket, aws_target_bucket, mpufile, objkey):
     util.mark_test('AWS MPU UPLOAD')
     aws_ep_bucket.create()
     util.upload_object(aws_ep_bucket, objkey, mpufile)
     util.remark('Done uploading file, downloading and checking hash')
-    assert util.check_object(objkey, mpufile, aws_ep_bucket, aws_target_bucket)
+    assert util.check_object(
+        objkey, mpufile, aws_ep_bucket, aws_target_bucket, timeout=60)
 
 
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.conformance
 def test_mpu_gcp(gcp_ep_bucket, gcp_target_bucket, mpufile, objkey):
     util.mark_test('GCP MPU UPLOAD')
     gcp_ep_bucket.create()
     util.upload_object(gcp_ep_bucket, objkey, mpufile)
     util.remark('Done uploading file, downloading and checking hash')
-    assert util.check_object(objkey, mpufile, gcp_ep_bucket, gcp_target_bucket)
+    assert util.check_object(
+        objkey, mpufile, gcp_ep_bucket, gcp_target_bucket, timeout=60)
 
 # @pytest.mark.skip()
 
 
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.conformance
 def test_mpu_azure(azure_ep_bucket, azure_target_bucket, mpufile, objkey):
     util.mark_test('AZURE MPU UPLOAD')
@@ -30,7 +35,7 @@ def test_mpu_azure(azure_ep_bucket, azure_target_bucket, mpufile, objkey):
     util.upload_object(azure_ep_bucket, objkey, mpufile)
     util.remark('Done uploading file, downloading and checking hash')
     assert util.check_object(
-        objkey, mpufile, azure_ep_bucket, azure_target_bucket)
+        objkey, mpufile, azure_ep_bucket, azure_target_bucket, timeout=60)
 
 
 @pytest.mark.skip(reason='Wasabi not implemented in CI')
