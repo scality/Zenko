194069176b6baedec7200313312ea4a621cc495f
Giacomo Guiulfo <giacomoguiulfo@gmail.com>
2018-07-20 10:22:26 -0700

    ci: re-enable grafana tests

--- tests/zenko_e2e/grafana/test_datasource.py
+++ tests/zenko_e2e/grafana/test_datasource.py
@@ -6,7 +6,6 @@ GRAFANA_DATASOURCES = [
 ]
 
 
-@pytest.mark.skip(reason="Grafana doesn't install on CI")
 @pytest.mark.nondestructive
 @pytest.mark.parametrize('datasource', GRAFANA_DATASOURCES)
 def test_datasource(grafana_client, datasource):
