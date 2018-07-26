a6368ccdcb35a8287b45bdabab0fdcda7004fd02
Taylor McKinnon <mail@tmacs.space>
2018-07-26 16:33:27 -0700

    feat(ZENKO-850): Enable GCP CRR test in eve

--- tests/backbeat/gcp_shim.sh
+++ tests/backbeat/gcp_shim.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-GCP_PRIV_KEY="$(echo $GCP_BACKEND_SERVICE_KEY | base64 -d | awk -f fix_priv_key.awk)"
-
-echo "Converted private key to:"
-echo "$GCP_PRIV_KEY"
+GCP_BACKEND_SERVICE_KEY="$(echo $GCP_BACKEND_SERVICE_KEY | tr -d '\n')"
 
 cat >gcp_key.json <<EOF
 {
-  "private_key": "${GCP_PRIV_KEY}",
+  "private_key": "${GCP_BACKEND_SERVICE_KEY}",
   "client_email": "${GCP_BACKEND_SERVICE_EMAIL}"
 }
 EOF
-
-exec $@
