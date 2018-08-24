1dc7ff6d8da7a1ce31dbdfb477cb7eb124697fb8
Salim <salim.salaues@scality.com>
2018-08-24 13:50:43 -0700

    bf: fix flaky test - ZENKO-1022
    Fixed with redis changes 1051

--- tests/node_tests/backbeat/tests/api/objectMonitor.js
+++ tests/node_tests/backbeat/tests/api/objectMonitor.js
@@ -2,7 +2,6 @@ const assert = require('assert');
 const crypto = require('crypto');
 const request = require('request');
 const { series, waterfall, doWhilst } = require('async');
-const tags = require('mocha-tags');
 
 const { scalityS3Client, awsS3Client } = require('../../../s3SDK');
 const ReplicationUtility = require('../../ReplicationUtility');
@@ -43,8 +42,7 @@ function getAndCheckResponse(path, expectedBody, cb) {
     () => shouldContinue, cb);
 }
 
-tags('flaky') // Tracking via ZENKO-1022
-.describe('Backbeat object monitor CRR metrics', function() {
+describe('Backbeat object monitor CRR metrics', function() {
     this.timeout(REPLICATION_TIMEOUT);
     let roleArn = 'arn:aws:iam::root:role/s3-replication-role';
 
