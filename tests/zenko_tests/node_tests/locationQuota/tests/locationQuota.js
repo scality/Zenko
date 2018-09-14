e1df1826ed8b071d0585e98946a2e9a817b50e19
Salim <salim.salaues@scality.com>
2018-09-14 16:18:56 -0700

    bugfix: re enable storage location quota tests

--- tests/zenko_tests/node_tests/locationQuota/tests/locationQuota.js
+++ tests/zenko_tests/node_tests/locationQuota/tests/locationQuota.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 const crypto = require('crypto');
 const async = require('async');
-const tags = require('mocha-tags');
 
 const { scalityS3Client } = require('../../s3SDK');
 const QuotaUtility = require('../QuotaUtility');
@@ -11,8 +10,7 @@ const bucket = `quota-bucket-${Date.now()}`;
 
 const scalityS3 = new QuotaUtility(scalityS3Client);
 
-tags('flaky') // Tracking via ZENKO-1034
-.describe('Location storage quota', () => {
+describe('Location storage quota', () => {
     beforeEach(function beFn(done) {
         this.timeout(50000);
         async.series([
