b87d9a9af2e0c9f498397e3cb7b1bb040f406467
Nicolas Trangez <ikke@nicolast.be>
2018-09-20 13:18:11 -0700

    docs: Ensure PDF can be built
    Special handling of SVG images is required.

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinxcontrib.spelling',
+    'sphinxcontrib.inkscapeconverter',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
