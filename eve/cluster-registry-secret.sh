f5506c281af37487486ddc408b28513e5087f56f
Thomas Carmet <thomas.carmet@scality.com>
2018-12-18 17:29:55 -0800

    ZENKO-653 fixup private registry encoding errors

---
+++ eve/cluster-registry-secret.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DOCKERCONFIG="$(cat ~/.docker/config.json | base64 | tr -d \\n)"
+
+cat >registry-secret.yml <<EOF
+apiVersion: v1
+data:
+  .dockerconfigjson: "${DOCKERCONFIG}"
+kind: Secret
+metadata:
+  name: registry
+  namespace: "${NAMESPACE}"
+type: kubernetes.io/dockerconfigjson
+EOF
+
+kubectl apply -f registry-secret.yml
+kubectl patch --namespace=${NAMESPACE} serviceaccount default -p '{"imagePullSecrets": [{"name": "registry"}]}'
