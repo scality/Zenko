10f6b3f99c2a6e75ecc1b49e917f21d83f831058
Thomas Carmet <thomas.carmet@scality.com>
2018-12-19 15:51:15 -0800

    feature: ZENKO-1405 setting up docker env for docs

--- docs/make.bat
+++ docs/make.bat
@@ -2,6 +2,15 @@
 
 pushd %~dp0
 
+REM docker build setup
+
+set DOCKER=docker
+set BUILDERNAME=zenko-docs
+set BUILDERIMAGE=..\
+set BUILDERDOCKERFILE=.\Dockerfile
+set BUILDERHOME=/usr/src/zenko
+
+
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
@@ -13,24 +22,17 @@ set SPHINXPROJ=Zenko
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
-	exit /b 1
-)
+REM create a docker container work environment
+:shell
+	%DOCKER% build -t %BUILDERNAME%:latest -f %BUILDERDOCKERFILE% %BUILDERIMAGE%
+	%DOCKER% run -it --rm -v "%~dp0\..:%BUILDERHOME%" -v "C:\:/c"--entrypoint=bash %BUILDERNAME%
+
+:help
+	%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 goto end
 
-:help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 
 :end
 popd
