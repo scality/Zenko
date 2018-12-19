d0b3aa1662253e8c2699c588076cca41ce36f108
Thomas Carmet <thomas.carmet@scality.com>
2018-12-19 15:51:15 -0800

    setting up docker env for documentation

--- docs/make.bat
+++ docs/make.bat
@@ -2,6 +2,15 @@
 
 pushd %~dp0
 
+REM docker build setup
+
+set DOCKER=docker
+set BUILDERNAME=zenko-docs
+set BUILDERIMAGE=..\
+set BUILDERDOCKERFILE=.\Dockerfile
+set BUILDERHOME=/usr/src/zenko
+
+
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
@@ -31,6 +40,10 @@ goto end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+
+:shell
+	%DOCKER% build -t %BUILDERNAME%:latest -f %BUILDERDOCKERFILE% %BUILDERIMAGE%
+	%DOCKER% run -it --rm -v %~dp0\..:%BUILDERHOME% --entrypoint=bash %BUILDERNAME%
 
 :end
 popd
