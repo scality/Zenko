330c9c75ce7405c9d5470bdf5642560a2eaa097e
Thomas Carmet <thomas.carmet@scality.com>
2018-12-20 13:43:10 -0800

    [tmp] ensure we can run make shell

--- docs/make.bat
+++ docs/make.bat
@@ -22,18 +22,18 @@ set SPHINXPROJ=Zenko
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
-	exit /b 1
-)
+REM %SPHINXBUILD% >NUL 2>NUL
+REM if errorlevel 9009 (
+REM 	echo.
+REM 	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+REM 	echo.installed, then set the SPHINXBUILD environment variable to point
+REM 	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+REM 	echo.may add the Sphinx directory to PATH.
+REM 	echo.
+REM 	echo.If you don't have Sphinx installed, grab it from
+REM 	echo.http://sphinx-doc.org/
+REM 	exit /b 1
+REM )
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 goto end
