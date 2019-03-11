10edb85efecdbf369d9ed1a998acb4005cf7bd7c
Jonathan Gramain <jonathan.gramain@scality.com>
2019-03-11 14:35:21 -0700

    feature: ZENKO-1383 enable transition policies E2E
    Update docker-entrypoint.sh to run lifecycle transition E2E tests.

--- tests/zenko_tests/docker-entrypoint.sh
+++ tests/zenko_tests/docker-entrypoint.sh
@@ -24,6 +24,6 @@ fi
 
 # Run the tests
 enter_and_run python_tests "./run.sh $PYTHON_ARGS"
-enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_location_quota test_bucket_get_v2"
+enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_lifecycle test_location_quota test_bucket_get_v2"
 
 exit "$EXIT_STATUS"
