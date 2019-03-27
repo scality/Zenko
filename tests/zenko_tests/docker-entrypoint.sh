9e659bc848979becd3957798d545c5195e25bdad
Salim <salim.salaues@scality.com>
2019-03-27 14:00:04 -0700

    ci: split tests into parallel stages

--- tests/zenko_tests/docker-entrypoint.sh
+++ tests/zenko_tests/docker-entrypoint.sh
@@ -20,7 +20,17 @@ if [ "$?" -ne "0" ]; then
 fi
 
 # Run the tests
-enter_and_run python_tests "./run.sh $PYTHON_ARGS"
-enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_location_quota test_bucket_get_v2"
+echo "Running test stage: $STAGE"
+if [ "$STAGE" = 'python-tests' ]; then
+    enter_and_run python_tests "./run.sh $PYTHON_ARGS"
+elif [ "$STAGE" = 'node-tests-01' ]; then
+    enter_and_run node_tests "npm_chain.sh test_aws_crr"
+elif [ "$STAGE" = 'node-tests-02' ]; then
+    enter_and_run node_tests "npm_chain.sh test_gcp_crr test_azure_crr test_one_to_many test_crr_pause_resume test_api test_location_quota test_bucket_get_v2"
+else
+    enter_and_run python_tests "./run.sh $PYTHON_ARGS"
+    # test_crr runs "test_aws_crr test_gcp_crr test_azure_crr test_one_to_many"
+    enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_location_quota test_bucket_get_v2"
+fi
 
 exit "$EXIT_STATUS"
