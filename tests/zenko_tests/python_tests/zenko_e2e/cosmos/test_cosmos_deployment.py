62f5c7456f58e87b0b52bd6d00ff81b73caf81c7
Salim <salim.salaues@scality.com>
2019-04-02 23:01:47 -0700

    feat: add cosmos e2e tests

---
+++ tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
@@ -0,0 +1,58 @@
+import logging
+import time
+import pytest
+import zenko_e2e.conf as conf
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+from ..fixtures import *
+from .. import util
+
+_log = logging.getLogger('cosmos')  # pylint: disable=invalid-name
+
+MD5_HASHES = {
+    "file1": "e61d15de7ad98164bad4394b818510a9",
+    "file2": "18bcf93f4a001b4cdfc3fc702847864f",
+    "file3": "b4bf46b4640547ab96e75b24170242c1",
+    "file4": "1baecec7b7c658357288ac736b6e95a6",
+    "file5": "465e3cc6ea85a2503de80459ad0b8634",
+}
+
+
+INGESTION_SUFFIX = '-cosmos-rclone-initial-ingest'
+
+
+@pytest.fixture
+def kube():
+    return client.ApiClient(config.load_incluster_config())
+
+
+@pytest.fixture
+def kube_batch(kube):
+    return client.BatchV1Api(kube)
+
+
+@pytest.mark.conformance
+def test_cosmos_nfs_ingestion(nfs_loc, nfs_loc_bucket, kube_batch, timeout=60):
+    util.mark_test('SOFS-NFS INGESTION')
+    job_name = nfs_loc + INGESTION_SUFFIX
+    _timestamp = time.time()
+
+    while time.time() - _timestamp < timeout:
+        try:
+            state = kube_batch.read_namespaced_job_status(
+                job_name, conf.K8S_NAMESPACE)
+            if state.status.succeeded:
+                assert state.status.succeeded
+                break
+        except ApiException as err:
+            _log.error("Exception when calling job status %s", err)
+        _log.info("Waiting for job completion")
+        time.sleep(1)
+    else:
+        _log.error('Initial ingestion did not complete in time')
+    _log.debug("Finished with job status %s", state)
+    assert state
+
+    for (key, md5) in MD5_HASHES.items():
+        _log.debug("Checking object %s with hash %s", key, md5)
+        assert util.get_object_hash(nfs_loc_bucket, key) == md5
