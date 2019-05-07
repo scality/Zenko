9ebbf009ae10e14af2ad4be2cef044be90fddd86
Taylor McKinnon <mail@tmacs.space>
2019-05-07 10:58:54 -0700

    bugfix(ZENKO-1779): Improve flaky ceph tests
    Add support for version ids to the local zenko check
    Refactor object existence checking to use boto3's builtin `wait_until_exists`
    Refactor object checking into seperate functions for simplicity

--- tests/zenko_tests/python_tests/zenko_e2e/util/bucket.py
+++ tests/zenko_tests/python_tests/zenko_e2e/util/bucket.py
@@ -4,6 +4,7 @@ import hashlib
 import tempfile
 import time
 from boto3.s3.transfer import TransferConfig
+from botocore.exceptions import ClientError, WaiterError
 import requests
 from awsauth import S3Auth
 import zenko_e2e.conf as conf
@@ -35,12 +36,15 @@ def enable_versioning(bucket):
     bucket.Versioning().enable()
 
 
-def get_object_hash(bucket, key, timeout=0, backoff=5, _timestamp=None):
+def get_object_hash(bucket, key, timeout=0, backoff=5, versionid=None, _timestamp=None):
     if _timestamp is None:
         _timestamp = time.time()
+    extra_args = {}
+    if versionid is not None:
+        extra_args['VersionId'] = versionid
     try:
         with tempfile.TemporaryFile() as tfile:
-            bucket.download_fileobj(key, tfile)
+            bucket.download_fileobj(key, tfile, ExtraArgs=extra_args)
             tfile.seek(0)
             return hashobj(tfile.read())
     except Exception as exp:  # pylint: disable=broad-except
@@ -55,30 +59,55 @@ def get_object_hash(bucket, key, timeout=0, backoff=5, _timestamp=None):
                            _timestamp=_timestamp, backoff=backoff)
 
 
-def check_object(key, data, local, *args, timeout=0, backoff=5):
-    ref_hash = hashobj(data)
-    local_hash = get_object_hash(local, key, timeout, backoff)
-    if local_hash is None:
-        _log.error('Unable to retrieve %s/%s from zenko', local.name, key)
-        contents = list(local.objects.all())
-        _log.debug('%s bucket contents %s', local.name, contents)
-    if ref_hash != local_hash:
-        _log.error('Local object hash != data hash')
+def wait_for_object(bucket, key, version_id=None, tries=1):
+    kwargs = {}
+    if version_id is not None:
+        kwargs['VersionId'] = version_id
+    for _ in range(tries):
+        try:
+            bucket.Object(key).wait_until_exists(**kwargs)
+            return True
+        except WaiterError:
+            pass
+    return False
+
+
+def get_object(bucket, key, version_id=None, tries=1):
+    if wait_for_object(bucket, key, version_id=version_id, tries=tries):
+        resp = bucket.Object(key).get()
+        return True, hashobj(resp['Body'].read())
+    return False, None
+
+
+def check_remote_object(bucket, key, ref_hash, version_id=None, tries=1):
+    success, remote_hash = get_object(bucket, key, version_id, tries)
+    if not success:
+        err = 'Failed to retrieve object from %s/%s' % (bucket.name, key)
+        _log.error(err)
+        return False, err
+    if ref_hash != remote_hash:
+        err = 'Reference hash != hash from %s/%s' % (bucket.name, key)
+        _log.error(err)
+        return False, err
+    return True, None
+
+
+def check_object(key, data, local, remote=[], version_id=None, tries=1):  # noqa pylint: disable=dangerous-default-value,too-many-arguments
+    if not isinstance(remote, list):
+        remote = [remote]
+    reference_hash = hashobj(data)
+    passed, error = check_remote_object(local, key, reference_hash,
+                                        version_id, tries)
+    if not passed:
+        _log.error(error)
         return False
-    passed = True
-    for bucket in args:
-        remotekey = '%s/%s' % (local.name,
-                key) if not conf.BUCKET_MATCH and local is not bucket else key  # noqa pylint: disable=bad-continuation
-        remote_hash = get_object_hash(bucket, remotekey, timeout, backoff)
-        if remote_hash is None:
-            _log.error('Unable to retrieve %s/%s from cloud backend',
-                       bucket.name, remotekey)
-            contents = list(bucket.objects.all())
-            _log.debug('%s bucket contents %s', bucket.name, contents)
-        if remote_hash != ref_hash:
-            _log.error('Object in %s did not match data!', bucket)
-            passed = False
-    return passed
+    for bucket in remote:
+        passed, error = check_remote_object(bucket, key, reference_hash,
+                                            tries=tries)
+        if not passed:
+            _log.error(error)
+            return False
+    return True
 
 
 def cleanup_bucket(bucket, replicated=False, delete_bucket=True): # noqa pylint: disable=too-many-locals
@@ -147,14 +176,23 @@ def cleanup_gcp_bucket(bucket, delete_bucket=False):
 UPLOAD_CONFIG = TransferConfig(multipart_threshold=10 * 1024 * 1024)  # 10MB
 
 
-def upload_object(bucket, key, data, **kwargs):
-    with tempfile.TemporaryFile() as upload:
+# Returns  <bool>, <str/None>
+#         success, version_id
+def upload_object(bucket, key, data, _retries=3, **kwargs):
+    obj = bucket.Object(key)
+    with tempfile.SpooledTemporaryFile() as upload:
         upload.write(data)
         upload.seek(0)
-        return bucket.upload_fileobj(upload,
-                                     key,
-                                     Config=UPLOAD_CONFIG,
-                                     ExtraArgs=kwargs)
+        # We use upload_fileobj as its a managed auto-mpu transfer
+        obj.upload_fileobj(upload, ExtraArgs=kwargs)
+    try:
+        obj.reload()  # Refresh object metadata from server
+        return True, obj.version_id
+    except ClientError:
+        _log.error('Falied to upload file, retrying... %i/3', str(3 - _retries))
+        if _retries > 0:
+            return upload_object(bucket, key, data, _retries=_retries - 1, **kwargs)
+    return False, None
 
 
 def get_from_preferred_read(bucket, key, location):
