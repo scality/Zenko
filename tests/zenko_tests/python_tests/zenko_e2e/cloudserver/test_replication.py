379bc85664ea2bd738636b1cb40d0687185ebdc8
Taylor McKinnon <mail@tmacs.space>
2019-05-10 14:15:42 -0700

    rework object uploading, downloading, and checking

--- tests/zenko_tests/python_tests/zenko_e2e/cloudserver/test_replication.py
+++ tests/zenko_tests/python_tests/zenko_e2e/cloudserver/test_replication.py
@@ -50,9 +50,14 @@ def test_ceph_1_1(
         ceph_crr_bucket, ceph_crr_target_bucket, objkey, datafile):
     util.mark_test('CEPH 1-1 REPLICATION')
     data = datafile()
-    util.upload_object(ceph_crr_bucket, objkey, data)
+    success, ver_id = util.upload_object(ceph_crr_bucket, objkey, data)
     assert util.check_object(
-        objkey, data, ceph_crr_bucket, ceph_crr_target_bucket, timeout=300)
+            objkey,
+            data,
+            ceph_crr_bucket,
+            [ceph_crr_target_bucket],
+            version_id=ver_id
+    )
 
 
 @pytest.mark.skip(reason='Wasabi not implemented in CI')
