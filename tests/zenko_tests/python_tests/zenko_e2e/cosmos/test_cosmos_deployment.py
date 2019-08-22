fed2e302490a0f36f4b15d7785f29aa0d4ff1a73
Salim <salim.salaues@scality.com>
2019-08-21 17:00:10 -0700

    ft: e2e aws oob tests

--- tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
+++ tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
@@ -10,11 +10,11 @@ from .. import util
 _log = logging.getLogger('cosmos')  # pylint: disable=invalid-name
 
 MD5_HASHES = {
-    "file1": "e61d15de7ad98164bad4394b818510a9",
-    "file2": "18bcf93f4a001b4cdfc3fc702847864f",
-    "file3": "b4bf46b4640547ab96e75b24170242c1",
-    "file4": "1baecec7b7c658357288ac736b6e95a6",
-    "file5": "465e3cc6ea85a2503de80459ad0b8634",
+    "file1": "e61d15de7ad98164bad4394b818510a9",  # 1KB
+    "file2": "18bcf93f4a001b4cdfc3fc702847864f",  # 1MB
+    "file3": "b4bf46b4640547ab96e75b24170242c1",  # 10MB
+    "file4": "1baecec7b7c658357288ac736b6e95a6",  # 100MB
+    "file5": "465e3cc6ea85a2503de80459ad0b8634",  # 1GB
 }
 
 
@@ -34,18 +34,15 @@ def kube_batch(kube):
 # Timeout has been increased to 180 because of setup time but really shouldn't
 # be increased any further. Please investigate possible regressions or test
 # refactor before increasing the timeout any further.
-@pytest.mark.conformance
-def test_cosmos_nfs_ingest(nfs_loc, nfs_loc_bucket, kube_batch, timeout=180):
-    util.mark_test('SOFS-NFS INGESTION')
-    job_name = INGESTION_JOB.format(nfs_loc)
+@pytest.fixture
+def wait_for_job(kube_batch, job_name, timeout=180):
     _timestamp = time.time()
-
     while time.time() - _timestamp < timeout:
         try:
             state = kube_batch.read_namespaced_job_status(
                 job_name, conf.K8S_NAMESPACE)
             if state.status.succeeded:
-                assert state.status.succeeded
+                _log.debug("Finished with job status %s", state)
                 break
         except ApiException as err:
             _log.error("Exception when calling job status %s", err)
@@ -53,9 +50,43 @@ def test_cosmos_nfs_ingest(nfs_loc, nfs_loc_bucket, kube_batch, timeout=180):
         time.sleep(1)
     else:
         _log.error('Initial ingestion did not complete in time')
-    _log.debug("Finished with job status %s", state)
-    assert state
+    return state
+
+
+@pytest.mark.conformance
+def test_cosmos_nfs_ingest(nfs_loc, nfs_loc_bucket, kube_batch):
+    util.mark_test('SOFS-NFS OOB INGESTION')
+    job_name = INGESTION_JOB.format(nfs_loc)
+    assert wait_for_job(kube_batch, job_name)
 
     for (key, md5) in MD5_HASHES.items():
         _log.debug("Checking object %s with hash %s", key, md5)
         assert util.get_object_hash(nfs_loc_bucket, key) == md5
+
+
+@pytest.mark.conformance
+def test_cosmos_aws_ingest(aws_target_bucket, zenko_bucket, kube_batch, testfile, objkey): # noqa pylint: disable=dangerous-default-value,too-many-arguments
+    util.mark_test('AWS OOB INGESTION')
+    aws_target_bucket.put_object(
+        Body=testfile,
+        Key=objkey,
+    )
+    zenko_bucket = aws_loc_bucket(zenko_bucket, ingest=True)
+    job_name = INGESTION_JOB.format(conf.AWS_BACKEND)
+    assert wait_for_job(kube_batch, job_name)
+    assert util.check_object(
+        objkey, testfile, zenko_bucket, aws_target_bucket)
+
+
+@pytest.mark.conformance
+def test_cosmos_ceph_ingest(ceph_target_bucket, zenko_bucket, kube_batch, testfile, objkey): # noqa pylint: disable=dangerous-default-value,too-many-arguments
+    util.mark_test('CEPH OOB INGESTION')
+    ceph_target_bucket.put_object(
+        Body=testfile,
+        Key=objkey,
+    )
+    zenko_bucket = ceph_loc_bucket(zenko_bucket, ingest=True)
+    job_name = INGESTION_JOB.format(conf.CEPH_BACKEND)
+    assert wait_for_job(kube_batch, job_name)
+    assert util.check_object(
+        objkey, testfile, zenko_bucket, ceph_target_bucket)
