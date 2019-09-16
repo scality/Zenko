58c7119ffb413d3d620eb5e057dc5f4817f43571
wabernatScality <william.abernathy@scality.com>
2019-09-16 13:03:28 -0700

    documentation: ZENKOIO-89_import_template_improvements
    This builds minimum required outputs; however, html/pdf trickery is not
    activated, so there's not much difference between html and pdf (i.e., no
    glossary or special pdf-only content).

--- docs/docsource/conf.py
+++ docs/docsource/conf.py
@@ -1,6 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file does only contain a selection of the most common options. For a
+# This file only contains a selection of the most common options. For a
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
@@ -21,24 +21,19 @@ doc_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(doc_dir))
 
 import scaldoc.latex
+import scaldoc.paths
 import scaldoc.resources
 
 # -- Project information -----------------------------------------------------
 
 project = 'Zenko'
-copyright = '2019, Scality, Inc'
-author = 'Scality Technical Publications'
+project_identifier = 'Zenko'
 
-# The short X.Y version
-version = '1.1'
-# The full version, including alpha/beta/rc tags
-release = '1.1'
-
+from projects_common_vars import author, copyright, release, version
 
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#
 # needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -50,8 +45,11 @@ extensions = [
             'sphinx.ext.ifconfig',
             'sphinxcontrib.spelling',
             'sphinxcontrib.inkscapeconverter',
-#            'sphinx.ext.autosectionlabel',
+#	    'sphinx_version_ref',
 ]
+
+plantuml_output_format = 'svg'
+plantuml_latex_output_format = 'pdf'
 
 autosectionlabel_prefix_document = True
 
@@ -61,8 +59,6 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
 
 source_suffix = '.rst'
 
@@ -84,6 +80,13 @@ language = None
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# if tags.has('html'):
+#     master_doc = 'index_pdf'
+#     exclude_patterns.extend(['index_pdf.rst', '*/index_pdf.rst'])
+# else:
+#     master_doc = 'index_pdf'
+#     exclude_patterns.extend(['*/index.rst', 'index.rst', '*/glossary.rst'])
+
 # The name of the Pygments (syntax highlighting) style to use.
 
 pygments_style = 'sphinx'
@@ -93,32 +96,31 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the
 # documentation for a list of builtin themes.
 
-html_theme = 'classic'
+html_theme = 'sphinx_scality'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'stickysidebar': True ,
-    'footerbgcolor': '#201921',
-    'footertextcolor': '#FFFFFF',
-    'sidebarlinkcolor': '#FFFFFF',
-    'sidebarbtncolor': '#201921',
-    'sidebarbgcolor': '#201921',
-    'sidebartextcolor': '#FFFFFF',
-    'relbarbgcolor': '#201921',
-    'relbartextcolor': '#FFFFFF',
-    'headbgcolor': '#FFFFFF',
-    'headtextcolor': '#000000',
-    'codebgcolor': '#F2F2F2' ,
-    'bodyfont': 'Roboto' ,
-    'headfont': 'Oswald' ,
+   'social_links': [
+      ("linkedin", "https://www.linkedin.com/company/scality/"),
+      ("twitter", "https://twitter.com/scality"),
+      ("instagram", "https://instagram.com/scalitylife"),
+      ("facebook", "https://www.facebook.com/scality/"),
+   ],
+   'footer_links': [
+      ("Support", "https://support.scality.com"),
+      ("Knowledge Base", "https://support.scality.com/hc/en-us"),
+      ("Training", "https://training.scality.com"),
+      ("Privacy Policy", "https://www.scality.com/privacy-policy/"),
+   ]
 }
 
 # add logo  (your logo goes in _static directory)
 
-html_logo = '_static/Zenko-Logo-Wide-white-on-sitegray.png'
+# html_logo = '_static/Zenko-Logo-Wide-white-on-sitegray.png'
+html_logo = '_static/{}_logo.svg'.format(project_identifier)
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -126,10 +128,18 @@ html_logo = '_static/Zenko-Logo-Wide-white-on-sitegray.png'
 
 html_static_path = ['_static']
 
+html_last_updated_fmt = '%B %d, %Y'
+
+# Extra variables to pass to templates
+
+html_context = {
+    'project_identifier': project_identifier,
+}
+
 # Favicon
 html_favicon = '_static/favicon.ico'
 
-# Custom sidebar templates, must be a dictionary that maps document names
+# Custom sidebar templates must be a dictionary that maps document names
 # to template names.
 #
 # The default sidebars (for documents that don't match any pattern) are
@@ -138,11 +148,9 @@ html_favicon = '_static/favicon.ico'
 # 'searchbox.html']``.
 #
 
-html_last_updated_fmt = '%B %d, %Y'
-
-html_sidebars = {
-     '**': ['globaltoc.html', 'relations.html', 'searchbox.html'],
-}
+# html_sidebars = {
+#     '**': ['globaltoc.html', 'relations.html', 'searchbox.html'],
+# }
 
 html_show_sphinx = False
 html_show_source = False
@@ -151,7 +159,7 @@ html_show_source = False
 
 # Output file base name for HTML help builder.
 
-htmlhelp_basename = 'ZenkoHelp'
+htmlhelp_basename = 'Zenko'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -160,14 +168,24 @@ latex_engine = 'xelatex'
 
 latex_contents = r"""
     \thispagestyle{empty}
-    \cleardoublepage
+    \clearpage
+    \setcounter{page}{1}
     \sphinxtableofcontents
 """
 
 latex_logo = scaldoc.resources.get_footer_logo()
 latex_cover = scaldoc.resources.get_cover('Zenko')
 
+if tags.has('install'):
+     latex_title = 'Installation'
+elif tags.has('operation'):
+     latex_title = 'Operation'
+elif tags.has('reference'):
+     latex_title = 'Reference'
+
 latex_elements = {
+     'extraclassoptions': 'openany,oneside',
+
     # The paper size ('letterpaper' or 'a4paper').
     #
      'papersize': 'letterpaper',
@@ -181,7 +199,7 @@ latex_elements = {
     'preamble': scaldoc.resources.get_latex_preamble(
         cover=os.path.basename(latex_cover),
         logo=os.path.basename(latex_logo),
-        title='Zenko',
+        title='latex_title',
         title_voffset='85pt',
         version=release,
         copyright=copyright
@@ -198,50 +216,42 @@ latex_additional_files.extend(scaldoc.resources.get_fonts())
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 
-latex_documents = [
-    (master_doc, 'ZenkoDocumentation.tex', 'Zenko Documentation',
-     'Scality Technical Publications', 'manual'),
-]
+# if tags.has('install'):
+#    latex_documents = [(
+#       'installation/index_pdf',
+#       'Zenko_Installation.tex',
+#       'Zenko Installation Documentation',
+#       'Scality Technical Publications',
+#       'manual'
+#    )]
+# elif tags.has('operation'):
+#    latex_documents = [(
+#       'operation/index_pdf',
+#       'Zenko_Operation.tex',
+#       'Zenko Operation Documentation',
+#       'Scality Technical Publications',
+#       'manual'
+#    )]
+# elif tags.has('reference'):
+#    latex_documents = [(
+#       'reference/index_pdf',
+#       'Zenko_Reference.tex',
+#       'Zenko Reference Documentation',
+#       'Scality Technical Publications',
+#       'manual'
+#    )]
 
-# -- Options for manual page output ------------------------------------------
+# Override the default formatter with our custom one.
 
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-# man_pages = [
-#    (master_doc, 'zenkooperationsguide', 'Zenko Operations Guide Documentation',
-#     [author], 1)
-# ]
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'ZenkoDocumentation', 'Zenko Documentation',
-    'Scality Technical Publications', 'Zenko Documentation', 'Documentation',
-     'manual'),
-]
-
-latex_engine = 'xelatex'
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-# epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-# epub_exclude_files = ['search.html']
-
-
+PygmentsBridge.latex_formatter = scaldoc.latex.Formatter
+ 
+ 
 def setup(app):
-        app.add_stylesheet('custom.css')
+     app.add_stylesheet('custom.css')
+     app.add_stylesheet('screenshot.css')
+
+# latex_documents = [
+#    (master_doc, 'ZenkoDocumentation.tex', 'Zenko Documentation',
+#    'Scality Technical Publications', 'manual'),
+#]
+
