44bd25bc80a4a572a8f54f7a7592d0e51ffb3f1f
Salim <salim.salaues@scality.com>
2019-10-08 12:03:47 -0700

    tests: add regression test to validate versionIDs

--- tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
+++ tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
@@ -35,6 +35,21 @@ def kube_corev1(kube):
     return client.CoreV1Api(kube)
 
 
+@pytest.fixture
+def compare_versions(objkey, aws_target_bucket, zenko_bucket):
+    src_obj = aws_target_bucket.Object(objkey)
+    dst_obj = zenko_bucket.Object(objkey)
+    if src_obj.version_id != dst_obj.metadata['version-id']:
+        return False
+    src_hash = util.get_object_hash(aws_target_bucket, objkey)
+    zenko_bucket.put_object(Key=objkey)
+    dst_hash = util.get_object_hash(
+        zenko_bucket, objkey, versionid=dst_obj.version_id)
+    if src_hash != dst_hash:
+        return False
+    return True
+
+
 # Timeout has been increased to 180 because of setup time but really shouldn't
 # be increased any further. Please investigate possible regressions or test
 # refactor before increasing the timeout any further.
@@ -77,9 +92,13 @@ def test_cosmos_aws_ingest(aws_target_bucket, zenko_bucket, kube_corev1, testfil
     )
     zenko_bucket = aws_loc_bucket(zenko_bucket, ingest=True)
     pod_name = INGESTION_POD.format(conf.AWS_BACKEND)
+    # Wait for initial ingestion
     assert wait_for_pod(kube_corev1, pod_name) == 'Succeeded'
+    # Validate ingestion
     assert util.check_object(
         objkey, testfile, zenko_bucket, aws_target_bucket)
+    # Validate versioning
+    assert compare_versions(objkey, aws_target_bucket, zenko_bucket)
 
 
 @pytest.mark.conformance
@@ -94,3 +113,4 @@ def test_cosmos_ceph_ingest(ceph_target_bucket, zenko_bucket, kube_corev1, testf
     assert wait_for_pod(kube_corev1, pod_name) == 'Succeeded'
     assert util.check_object(
         objkey, testfile, zenko_bucket, ceph_target_bucket)
+    assert compare_versions(objkey, ceph_target_bucket, zenko_bucket)
