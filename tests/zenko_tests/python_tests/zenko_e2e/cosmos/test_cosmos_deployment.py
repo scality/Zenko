50918cb3d94dfc8ad145725f2f5e3e8bb282fe41
Michael Rutledge <r@scality.com>
2021-06-08 16:57:41 -0700

    Updating s3 timeout from 180 to 360 for cosmos tests

--- tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
+++ tests/zenko_tests/python_tests/zenko_e2e/cosmos/test_cosmos_deployment.py
@@ -73,8 +73,10 @@ def compare_versions(objkey, aws_target_bucket, zenko_bucket):
 # Timeout has been increased to 180 because of setup time but really shouldn't
 # be increased any further. Please investigate possible regressions or test
 # refactor before increasing the timeout any further.
+# Updated timeout to 360 to allow for increased system latency 
+
 @pytest.fixture
-def wait_for_job(kube_batch, location, timeout=180):
+def wait_for_job(kube_batch, location, timeout=360):
     _timestamp = time.time()
     while time.time() - _timestamp < timeout:
         try:
