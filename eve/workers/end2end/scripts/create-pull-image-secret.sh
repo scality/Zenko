513a57f50c87ef9bc272a431258e0173af5d8554
anurag4DSB <anurag.mittal@scality.com>
2021-07-29 11:12:21 +0200

    feature: ZENKO-3320-add-vault-end-2-end-tests
    * Adds end-2-end tests for vault
    * updates vault tag to 8.2.3
    * TODO: Add keycloak and AssumeRoleWWI tests in the next vault release

--- eve/workers/end2end/scripts/create-pull-image-secret.sh
+++ eve/workers/end2end/scripts/create-pull-image-secret.sh
@@ -5,3 +5,5 @@ set -exu
 kubectl create secret generic zenko-operator-image-pull --dry-run -o yaml \
     --from-file=.dockerconfigjson=$HOME/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson | kubectl apply -f -
+
+ kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "zenko-operator-image-pull"}]}'
