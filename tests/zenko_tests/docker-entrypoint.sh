4f861b7c587c88b7d6d9411e419f80b8b7a93909
Nicolas Humbert <nicolas.humbert@scality.com>
2021-10-05 11:19:02 -0400

    lifecycle expiration tests fail

--- tests/zenko_tests/docker-entrypoint.sh
+++ tests/zenko_tests/docker-entrypoint.sh
@@ -27,7 +27,7 @@ echo "Running test stage: $STAGE"
 if [ "$STAGE" = 'python-tests' ]; then
     enter_and_run python_tests "./run.sh $PYTHON_ARGS"
 elif [ "$STAGE" = 'node-tests-01' ]; then
-    enter_and_run node_tests "npm_chain.sh test_aws_crr test_api test_location_quota test_bucket_get_v2 test_ingestion_oob_s3c test_bucket_policy"
+    enter_and_run node_tests "npm_chain.sh test_lifecycle"
 elif [ "$STAGE" = 'node-tests-02' ]; then
     enter_and_run node_tests "npm_chain.sh test_gcp_crr test_azure_crr test_one_to_many test_lifecycle test_crr_pause_resume"
 else
