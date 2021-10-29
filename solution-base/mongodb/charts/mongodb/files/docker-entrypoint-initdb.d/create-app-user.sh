0fedba4c1a8d02be42a0ab5ddfb8f2efd74ab9aa
Alexander Chan <alexander.chan@scality.com>
2021-10-29 08:45:27 -0700

    ZENKO-3700: fix create-user script to fail with non-zero

--- solution-base/mongodb/charts/mongodb/files/docker-entrypoint-initdb.d/create-app-user.sh
+++ solution-base/mongodb/charts/mongodb/files/docker-entrypoint-initdb.d/create-app-user.sh
@@ -24,7 +24,10 @@ retry() {
         sleep 5
     done
 
-    [ $count -lt 10 ] || echo "Failed to create app user."
+    if [ $count -ge 10 ]; then
+        echo "Failed to create app user."
+        exit 1
+    fi
 }
 
 retry create_user
