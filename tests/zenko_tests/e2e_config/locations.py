729eece56bf3141307eeef5ba04ad00aed926d32
Naren <narendran.rajendran@scality.com>
2021-11-05 03:21:36 -0700

    improvement: ZENKO-3661 backbeat replication tests configuration

--- tests/zenko_tests/e2e_config/locations.py
+++ tests/zenko_tests/e2e_config/locations.py
@@ -4,5 +4,28 @@ import logging
 
 _log = logging.getLogger("end2end configuration")
 
-def create_locations(client, UUID, location):
-    pass
+def create_location(client, uuid, location):
+    """
+    Creates a location
+    :param client: swagger client
+    :param uuid: zenko instance uuid
+    :param location: location details
+    """
+    try:
+        Location_V1 = client.get_model('location-v1')
+        loc = Location_V1(name=location["name"],
+                            locationType=location["locationType"],
+                            details=location["details"])
+
+        res = (
+            client.ui_facing
+            .createConfigurationOverlayLocation(location=loc, uuid=uuid)
+            .response()
+            .result
+        )
+
+        _log.info(res)
+        _log.info("location created")
+    except Exception as e:
+        raise Exception(
+            "Failed to create location '%s': %s" % (location["name"], e))
