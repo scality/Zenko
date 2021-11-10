37351331fd9345dcdaa4a4caea8145d5a93497c3
Rached Ben Mustapha <rached.ben-mustapha@scality.com>
2021-11-09 16:10:33 -0800

    bugfix: add e2e tests for unicode object keys

---
+++ tests/zenko_tests/python_tests/zenko_e2e/cloudserver/test_crud.py
@@ -0,0 +1,11 @@
+from ..fixtures import *
+
+
+def test_put_unicode(zenko_bucket, testfile, objkey_unicode):
+    util.mark_test('CRUD')
+    bucket = zenko_bucket  # pylint: disable=unused-variable
+    bucket.put_object(
+        Body=testfile,
+        Key=objkey_unicode
+    )
+    assert util.check_object(objkey, testfile, bucket)
