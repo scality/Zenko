14b6c8de8ff4d95b3a210d550d517936287364e1
Alexander Chan <alexander.chan@scality.com>
2021-11-12 14:13:39 -0800

    mock changes

--- tests/zenko_tests/e2e_config/locations.py
+++ tests/zenko_tests/e2e_config/locations.py
@@ -13,6 +13,8 @@ def create_location(client, uuid, location):
     """
     try:
         Location_V1 = client.get_model('location-v1')
+        if "bootstrapList" not in location["details"]:
+            location["details"]["bootstrapList"] = []
         loc = Location_V1(name=location["name"],
                             locationType=location["locationType"],
                             details=location["details"])
@@ -24,7 +26,6 @@ def create_location(client, uuid, location):
             .result
         )
 
-        _log.info(res)
         _log.info("location created")
     except Exception as e:
         raise Exception(
