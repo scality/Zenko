e9b8567ae0604771861122536828fe7263c95cb4
Naren <narendran.rajendran@scality.com>
2021-11-17 15:24:11 -0800

    [debug] check secrets

--- tests/zenko_tests/e2e_config/locations.py
+++ tests/zenko_tests/e2e_config/locations.py
@@ -12,6 +12,8 @@ def create_location(client, uuid, location):
     :param location: location details
     """
     try:
+        print('LOCATION---->')
+        print(location)
         Location_V1 = client.get_model('location-v1')
         if "bootstrapList" not in location["details"]:
             location["details"]["bootstrapList"] = []
