999bab7941361cc79f59fc16803945b75268b63b
Nicolas Humbert <nicolas.humbert@scality.com>
2022-01-27 21:49:25 -0500

    try

--- tests/zenko_tests/docker-entrypoint.sh
+++ tests/zenko_tests/docker-entrypoint.sh
@@ -13,22 +13,6 @@ enter_and_run() {
     cd "$old_cwd"
 }
 
-# Setup our environment
-# TODO: use node js to create and remove buckets
-python3 create_buckets.py
-if [ "$?" -ne "0" ]; then
-    exit 1
-fi
-
-# locations need to be created after the location's bucket creation.
-python3 create_locations.py
-if [ "$?" -ne "0" ]; then
-    exit 1
-fi
-
-# wait locations propagate
-sleep 60
-
 # Run the tests
 echo "Running test stage: $STAGE"
 
@@ -37,17 +21,31 @@ if [ "$STAGE" = "end2end" ]; then
 elif [ "$STAGE" = "smoke" ]; then
     enter_and_run node_tests "npm_chain.sh test_smoke"
 elif [ "$STAGE" = "backbeat" ]; then
+    # TODO: use node js to create and remove buckets
+    python3 create_buckets.py
+    if [ "$?" -ne "0" ]; then
+        exit 1
+    fi
+    # locations need to be created after the location's bucket creation.
+    python3 create_locations.py
+    if [ "$?" -ne "0" ]; then
+        exit 1
+    fi
+
+    # wait locations propagate
+    sleep 60
+
     enter_and_run node_tests "npm_chain.sh test_aws_crr test_ingestion_oob_s3c"
-fi
 
-python3 delete_locations.py
-if [ "$?" -ne "0" ]; then
-    exit 1
-fi
+    python3 delete_locations.py
+    if [ "$?" -ne "0" ]; then
+        exit 1
+    fi
 
-python3 cleans3c.py
-if [ "$?" -ne "0" ]; then
-    exit 1
+    python3 cleans3c.py
+    if [ "$?" -ne "0" ]; then
+        exit 1
+    fi
 fi
 
 exit "$EXIT_STATUS"
