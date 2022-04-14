7af040b8f8caf454281fc8a8793529e68ceb411e
Alexander Chan <alexander.chan@scality.com>
2022-04-13 22:06:13 -0700

    ZENKO-4099: set mongodb default write concern to majority

---
+++ eve/workers/end2end/scripts/configs/mongodb_init_scripts/set-default-write-concern-majority.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set_default_majority() {
+    mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}"  -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
+conf = rs.config()
+
+defaultWriteConcern = conf.settings.getLastErrorDefaults
+
+if (!defaultWriteConcern || defaultWriteConcern.w !== 'majority') {
+    defaultWriteConcern.w = 'majority'
+    rs.reconfig(conf)
+}
+EOF
+}
+
+set_default_majority
---
+++ solution-base/mongodb/charts/mongodb/files/docker-entrypoint-initdb.d/set-default-write-concern-majority.sh
@@ -0,0 +1,1 @@
+../../../../../../eve/workers/end2end/scripts/configs/mongodb_init_scripts/set-default-write-concern-majority.sh
