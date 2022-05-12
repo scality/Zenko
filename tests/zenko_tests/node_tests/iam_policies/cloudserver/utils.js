dcb229621adf7a311339c5823bb4b5ec3fe19611
williamlardier <william.lardier@scality.com>
2022-05-12 11:20:29 +0200

    ZENKO-4143: implement restoreobject test and rework utils file

--- tests/zenko_tests/node_tests/iam_policies/cloudserver/utils.js
+++ tests/zenko_tests/node_tests/iam_policies/cloudserver/utils.js
@@ -1,8 +1,27 @@
-const { makeGETRequest, getResponseBody } = require('../../utils/request');
+const { makeGETRequest, getResponseBody, makePOSTRequest } = require('../../utils/request');
 
-function metadataSearchResponseCode(userCredentials, bucketName, cb) {
-    return makeGETRequest(`/${bucketName}/?search=${encodeURIComponent('key LIKE "file"')}`, (err, response) => {
-        if (err) { return cb(err); }
+function makeApiCallGeneric(mode = 'GET', body, userCredentials, query, cb) {
+    const fn = mode === 'GET' ? makeGETRequest : makePOSTRequest;
+    if (mode === 'GET') {
+        return makeGETRequest(query, (err, response) => {
+            if (err) {
+                return cb(err);
+            }
+            const { statusCode } = response;
+            return getResponseBody(response, (err, res) => {
+                const r = /<Message>(.*)<\/Message>/;
+                const message = res.match(r);
+                if (message !== null) {
+                    return cb(null, { statusCode, message: message[1] });
+                }
+                return cb(null, { statusCode, message });
+            }, true);
+        }, userCredentials);
+    }
+    return fn(query, body, (err, response) => {
+        if (err) {
+            return cb(err);
+        }
         const { statusCode } = response;
         return getResponseBody(response, (err, res) => {
             const r = /<Message>(.*)<\/Message>/;
@@ -15,6 +34,28 @@ function metadataSearchResponseCode(userCredentials, bucketName, cb) {
     }, userCredentials);
 }
 
+function metadataSearchResponseCode(userCredentials, bucketName, cb) {
+    return makeApiCallGeneric(
+        'GET',
+        null,
+        userCredentials,
+        `/${bucketName}/?search=${encodeURIComponent('key LIKE "file"')}`,
+        cb);
+}
+
+function restoreObjectResponseCode(userCredentials, bucketName, cb) {
+    return makeApiCallGeneric(
+        'POST',
+        {
+            RestoreDay: 1,
+            Days: 1,
+        },
+        userCredentials,
+        `/${bucketName}/restore`,
+        cb);
+}
+
 module.exports = {
     metadataSearchResponseCode,
+    restoreObjectResponseCode,
 };
