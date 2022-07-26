5e572fe20e33dafb5f0c88ae70238b2b9e1a8257
Naren <narendran.rajendran@scality.com>
2022-07-26 14:33:56 -0700

    [debug] test scripts

--- tests/zenko_tests/node_tests/backbeat/tests/lifecycle/transition.js
+++ tests/zenko_tests/node_tests/backbeat/tests/lifecycle/transition.js
@@ -1,3 +1,6 @@
+const assert = require('assert');
+const async = require('async');
+
 const uuid = require('uuid/v4');
 const { series } = require('async');
 
@@ -58,6 +61,7 @@ const locationParams = {
         name: process.env.COLD_BACKEND_DESTINATION_LOCATION,
         supportsVersioning: true,
         isCold: true,
+        supportsRestore: true,
     },
 };
 
@@ -71,7 +75,7 @@ const testsToRun = [{
 }];
 
 testsToRun.forEach(test => {
-    describe(`Lifecycle transition from ${test.from} to ${test.to}`, () => {
+    describe.skip(`Lifecycle transition from ${test.from} to ${test.to}`, () => {
         const srcBucket = `transition-bucket-${uuid()}`;
         const keyPrefix = uuid();
         const cloudServer = new LifecycleUtility(scalityS3Client)
@@ -180,5 +184,103 @@ testsToRun.forEach(test => {
                 });
             });
         }
+    });
+});
+
+const test = {
+    from: 'LocalStorage',
+    to: 'DMF',
+};
+
+describe('Restore', () => {
+    const srcBucket = `transition-bucket-${uuid()}`;
+    const keyPrefix = uuid();
+    const cloudServer = new LifecycleUtility(scalityS3Client)
+        .setBucket(srcBucket)
+        .setKeyPrefix(keyPrefix);
+    const cloud = new LifecycleUtility(awsS3Client).setKeyPrefix(keyPrefix);
+    const fromLoc = locationParams[test.from];
+    const toLoc = locationParams[test.to];
+    const prefix = `${keyPrefix}-from-${test.from}-to-${test.to}-`;
+
+    before(() => {
+        cloudServer.setSourceLocation(fromLoc.name);
+        cloudServer.setDestinationLocation(toLoc.name);
+        if (!toLoc.isCold) {
+            cloud.setLocationType(test.to);
+            cloud.setBucket(toLoc.bucket);
+        }
+    });
+
+    afterEach(done => series([
+        next => cloudServer.deleteVersionedBucket(srcBucket, next),
+        next => {
+            if (toLoc.isCold) {
+                return next();
+            }
+            return cloud.clearBucket(next);
+        },
+    ], done));
+
+    describe('without versioning', () => {
+        beforeEach(done => cloudServer.createBucket(srcBucket, done));
+
+        it('should transition a 0 byte object', done => {
+            const key = `${prefix}nover-object`;
+            cloudServer.setKey(key);
+            cloud.setKey(`${srcBucket}/${key}`);
+            series([
+                next => cloudServer.putObject(Buffer.from('test-object'), next),
+                next => checkTransition(toLoc, cloudServer, cloud, null, next),
+                next => cloudServer.getObject(next),
+                next => cloudServer.putBucketLifecycleConfiguration(new Date(), next),
+                next => cloudServer.waitUntilTransitioned(null, next),
+                // get object from source - should throw error
+                next => cloudServer.getObject(err => {
+                    assert.strictEqual(err.code, 'InvalidObjectState');
+                    assert.strictEqual(err.statusCode, 403);
+                    next();
+                }),
+                // issue restore
+                next => cloudServer.s3.restoreObject({
+                    Bucket: srcBucket,
+                    Key: key,
+                    RestoreRequest: {
+                        Days: 1,
+                        Tier: 'Standard',
+                    },
+                }, (err, data) => {
+                    if (err) {
+                        console.log('ERROR###', err);
+                    } else {
+                        console.log('DATA###', data);
+                    }
+                    next();
+                }),
+                // check if restored
+                next => {
+                    let shouldContinue;
+                    const params = {
+                        Bucket: srcBucket,
+                        Key: key,
+                    };
+                    return async.doWhilst(
+                        nxt => cloudServer.s3.headObject(params, (err, data) => {
+                            if (err) {
+                                return nxt(err);
+                            }
+                            shouldContinue = data.StorageClass !== fromLoc.name;
+                            if (shouldContinue) {
+                                return setTimeout(nxt, 5000);
+                            }
+                            return nxt();
+                        }),
+                        () => shouldContinue,
+                        next,
+                    );
+                },
+                // check data
+            ], done);
+        });
     });
 });
