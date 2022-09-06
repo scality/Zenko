1a9115d0b366f653e98d31261e9a78433be5613e
Rached Ben Mustapha <rached.ben-mustapha@scality.com>
2022-09-06 23:54:37 +0000

    update local manifest copy

--- solution-base/redeploy.sh
+++ solution-base/redeploy.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
 ./build.sh
+
+sed -e s/SOLUTION_ENV/zenko/g -e s/MONGODB_STORAGE_CLASS/ssd-ext4/g < _build/root/deploy/mongodb-sharded-9-nodes.yaml > mongodb-sharded-9-nodes.yaml
+
 kubectl kustomize . | kubectl apply -f - -n zenko
