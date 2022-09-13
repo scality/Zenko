a08d0e50b37598c1a2feccd6696dc89d5f8bf9ac
williamlardier <william.lardier@scality.com>
2022-09-13 09:22:29 +0200

    ZENKO-4273: remove eve folder

--- eve/workers/end2end/scripts/create-pull-image-secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -exu
-
-kubectl create secret generic zenko-operator-image-pull --dry-run -o yaml \
-    --from-file=.dockerconfigjson=$HOME/.docker/config.json \
-    --type=kubernetes.io/dockerconfigjson | kubectl apply -f -
-
- kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "zenko-operator-image-pull"}]}'
