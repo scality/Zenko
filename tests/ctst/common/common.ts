01cfd0fff7954638370999dae4e978b168468738
Will Toozs <william.toozshobson@scality.com>
2023-02-14 11:17:41 +0100

    update common functions

--- tests/ctst/common/common.ts
+++ tests/ctst/common/common.ts
@@ -18,16 +18,34 @@ Given('a {string} bucket', async function (versioning) {
     }
 });
 
-Given('an existing bucket {string} an object', async function (isObject) {
+Given('an existing bucket {string} {string} versioning', async function (bucketName, hasVersioning) {
+    this.resetCommand();
     const preName = this.parameters.AccountName || Constants.ACCOUNT_NAME;
-    const bucketName = `${preName}${Constants.BUCKET_NAME_TEST}${Utils.randomString()}`.toLocaleLowerCase();
+    if (bucketName === '') {
+        bucketName = `${preName}${Constants.BUCKET_NAME_TEST}${Utils.randomString()}`.toLocaleLowerCase();
+    }
     this.saved.bucketName = bucketName;
     this.addCommandParameter({ bucket: bucketName });
     await S3.createBucket(this.getCommandParameters());
-    if (isObject === 'with') {
-        const objectName = 'x'.repeat(10);
+    if (hasVersioning === 'with') {
+        this.addCommandParameter({ versioningConfiguration: `Status=Enabled`});
+        await S3.putBucketVersioning(this.getCommandParameters());
+    }
+});
+
+Given('we {string} an existing object {string} {string} version-Id {string}',
+    async function (have, objectName, hasVersioning, versionId) {
+    this.resetCommand();
+    if (have === 'have') {
+        if (objectName === '') {
+            objectName = 'x'.repeat(10);
+        }
         this.saved.objectName = objectName;
         this.addCommandParameter({key: objectName});
+        this.addCommandParameter({bucket: this.saved.bucketName});
+        if (hasVersioning === 'with') {
+            this.addCommandParameter({versionId});
+        }
         await S3.putObject(this.getCommandParameters());
     }
 });
