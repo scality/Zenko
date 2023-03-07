13295494b0a5687f9fa1fc3f2f41c3ddd39299b9
Will Toozs <william.toozshobson@scality.com>
2023-03-07 15:11:39 +0100

    add test tag

--- .github/scripts/end2end/run-e2e-ctst.sh
+++ .github/scripts/end2end/run-e2e-ctst.sh
@@ -53,4 +53,4 @@ kubectl run $POD_NAME \
         --attach=True \
         --image-pull-policy=Always \
         --env=TARGET_VERSION=$VERSION  \
-        -- ./run "$COMMAND" $WORLD_PARAMETERS "--parallel $PARALLEL_RUNS --retry $RETRIES --retry-tag-filter @Flaky"
+        -- ./run "$COMMAND" $WORLD_PARAMETERS "--parallel $PARALLEL_RUNS --retry $RETRIES --retry-tag-filter @Flaky --tags @IAM-Policies-ARWWI"
