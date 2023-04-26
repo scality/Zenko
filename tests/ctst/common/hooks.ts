ab7083308736788dc3ca9fc6da9b3506db7ea6e7
Will Toozs <william.toozshobson@scality.com>
2023-04-26 18:02:59 +0200

    ZENKO-4523: lint common files

--- tests/ctst/common/hooks.ts
+++ tests/ctst/common/hooks.ts
@@ -4,15 +4,20 @@ import Zenko, { EntityType } from '../world/Zenko';
 // HTTPS should not cause any error for CTST
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const worlds : any = {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const worlds : any = { // TODO: World doesn't work here becuase Zenko is missing some properties, what to do ?
     ZENKO: Zenko,
 };
 
 Before(async function () {
+    // TODO related to above
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call 
     await worlds[process.env.MODE ?? 'ZENKO'].init(this.parameters);
 });
 
-AfterAll(async function () {
+AfterAll(async () => {
+    // TODO related to above
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     await worlds[process.env.MODE ?? 'ZENKO'].teardown();
 });
 
@@ -22,18 +27,19 @@ defineParameterType({
     transformer: s => <keyof typeof EntityType> s,
 });
 
-Given('a {type} type', async function (type) {
+Given('a {type} type', async function (this: Zenko, type: string) {
     await this.setupEntity(type);
 });
 
-Given('a {string} AssumeRole user', async function (crossAccount: string) {
+Given('a {string} AssumeRole user', async function (this: Zenko, crossAccount: string) {
     await this.prepareAssumeRole(crossAccount === 'cross account');
-    this.saved.type = EntityType.ASSUME_ROLE_USER;
+    this.addToSaved('type', EntityType.ASSUME_ROLE_USER);
 });
 
-Given('a service user {string} assuming role {string}', async function (serviceUserName: string, roleName: string) {
-    await this.prepareServiceUser(serviceUserName, roleName);
-    this.saved.type = EntityType.ASSUME_ROLE_USER;
-});
+Given('a service user {string} assuming role {string}',
+    async function (this: Zenko, serviceUserName: string, roleName: string) {
+        await this.prepareServiceUser(serviceUserName, roleName);
+        this.addToSaved('type', EntityType.ASSUME_ROLE_USER);
+    });
 
 export default worlds;
--- tests/ctst/common/utils.ts
+++ tests/ctst/common/utils.ts
@@ -8,15 +8,14 @@ import {
  * @param {string[]} propertyChain - the property chain to extract, like Policy, Arn
  * @return {string} - the expected property, or null if an error occurred when parsing results.
  */
-export function extractPropertyFromResults<T>
-(results: Utils.Command, ...propertyChain: string[]) : T | null {
+export function extractPropertyFromResults<T>(results: Utils.Command, ...propertyChain: string[]) : T | null {
     try {
         if (results.stdout) {
-            const jsonResults = JSON.parse(results.stdout);
-            let res = jsonResults;
+            const jsonResults = JSON.parse(results.stdout) as Record<string, unknown>;
+            let res : unknown = jsonResults;
             if (jsonResults) {
                 while (propertyChain.length > 0) {
-                    res = res[propertyChain.shift() as string];
+                    res = (res as Record<string, unknown>)[propertyChain.shift()!];
                 }
             }
             return res as T;
@@ -27,52 +26,54 @@ export function extractPropertyFromResults<T>
     }
 }
 
-export const s3FunctionExtraParams : { [key: string]: Object } = {
-    'restoreObject': { restoreRequest: 'Days=1' },
-    'putObjectAcl':  { acl: 'public-read-write' },
-    'putBucketTagging': { tagging: 'TagSet=[{Key=tag1,Value=value1},{Key=tag2,Value=value2}]' },
-    'putObjectTagging': { tagging: 'TagSet=[{Key=string,Value=string}]' },
-    'putBucketVersioning': { versioningConfiguration: 'Status=Enabled' },
-    'putObjectLockConfiguration': {
-        objectLockConfiguration: '{ "ObjectLockEnabled": "Enabled", "Rule": { "DefaultRetention": { "Mode": "COMPLIANCE", "Days": 50 }}}'
+export const s3FunctionExtraParams : { [key: string]: Record<string, unknown> } = {
+    restoreObject: { restoreRequest: 'Days=1' },
+    putObjectAcl:  { acl: 'public-read-write' },
+    putBucketTagging: { tagging: 'TagSet=[{Key=tag1,Value=value1},{Key=tag2,Value=value2}]' },
+    putObjectTagging: { tagging: 'TagSet=[{Key=string,Value=string}]' },
+    putBucketVersioning: { versioningConfiguration: 'Status=Enabled' },
+    putObjectLockConfiguration: {
+        objectLockConfiguration: '{ "ObjectLockEnabled": "Enabled", "Rule": ' +
+        '{ "DefaultRetention": ' + 
+        '{ "Mode": "COMPLIANCE", "Days": 50 }}}',
     },
-    'deleteObjects': {
-        delete: `Objects=[{Key=${'x'.repeat(10)}}]`
+    deleteObjects: {
+        delete: `Objects=[{Key=${'x'.repeat(10)}}]`,
     },
-    'putBucketLifecycleConfiguration': {
+    putBucketLifecycleConfiguration: {
         lifecycleConfiguration: JSON.stringify(
             {
                 Rules: [
                     {
-                        Prefix: "",
-                        Status: "Enabled",
+                        Prefix: '',
+                        Status: 'Enabled',
                         Transitions: [
                             {
                                 Days: 365,
-                                StorageClass: "e2e-cold"
-                            }
+                                StorageClass: 'e2e-cold',
+                            },
                         ],
                         Expiration: {
-                            Days: 3650
+                            Days: 3650,
                         },
-                        ID: "ExampleRule"
-                    }
-                ]
-            })
+                        ID: 'ExampleRule',
+                    },
+                ],
+            }),
     },
-    'putBucketReplication': {
+    putBucketReplication: {
         replicationConfiguration: JSON.stringify(
-                {
-                    "Role": "arn:aws:iam::123456789012:role/s3-replication-role",
-                    "Rules": [
-                        {
-                            "Status": "Enabled",
-                            "Prefix": "",
-                            "Destination": {
-                                "Bucket": "arn:aws:s3:::examplebucket",
-                            }
-                        }
-                    ]
-                })
-    }
-}
+            {
+                Role: 'arn:aws:iam::123456789012:role/s3-replication-role',
+                Rules: [
+                    {
+                        Status: 'Enabled',
+                        Prefix: '',
+                        Destination: {
+                            Bucket: 'arn:aws:s3:::examplebucket',
+                        },
+                    },
+                ],
+            }),
+    },
+};
