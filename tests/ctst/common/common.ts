5f07e0c0304ce3891e256281c36c2a4567e9abcd
Will Toozs <william.toozshobson@scality.com>
2023-05-09 15:55:08 +0200

    ZENKO-4523: lint common files

--- tests/ctst/common/common.ts
+++ tests/ctst/common/common.ts
@@ -1,17 +1,18 @@
 import { Given, setDefaultTimeout } from '@cucumber/cucumber';
 import { Constants, S3, Utils } from 'cli-testing';
+import Zenko from 'world/Zenko';
 import { extractPropertyFromResults } from './utils';
 
 setDefaultTimeout(Constants.DEFAULT_TIMEOUT);
 
-Given('a {string} bucket', async function (versioning) {
+Given('a {string} bucket', async function (this: Zenko, versioning: string) {
     this.resetCommand();
     const preName = this.parameters.AccountName || Constants.ACCOUNT_NAME;
     const bucketName = `${preName}${Constants.BUCKET_NAME_TEST}${Utils.randomString()}`.toLocaleLowerCase();
-    this.saved.bucketName = bucketName;
+    this.addToSaved('bucketName', bucketName);
     this.addCommandParameter({ bucket: bucketName });
     await S3.createBucket(this.getCommandParameters());
-    this.saved.bucketVersioning = versioning;
+    this.addToSaved('bucketVersioning', versioning);
     if (versioning !== 'Non versioned') {
         const versioningConfiguration = versioning === 'Versioned' ? 'Enabled' : 'Suspended';
         this.addCommandParameter({ versioningConfiguration: `Status=${versioningConfiguration}` });
@@ -20,40 +21,41 @@ Given('a {string} bucket', async function (versioning) {
 });
 
 Given('an existing bucket {string} {string} versioning, {string} ObjectLock {string} retention mode', async function
-    (bucketName, withVersioning, withObjectLock, retentionMode) {
+(this: Zenko, bucketName: string, withVersioning: string, withObjectLock: string, retentionMode: string) {
     this.resetCommand();
-    const preName = this.parameters.AccountName || Constants.ACCOUNT_NAME;
-    bucketName = bucketName || `${preName}${Constants.BUCKET_NAME_TEST}${Utils.randomString()}`.toLocaleLowerCase();
-    this.saved.bucketName = bucketName;
-    this.addCommandParameter({ bucket: bucketName });
+    const preName = (this.parameters.AccountName || Constants.ACCOUNT_NAME);
+    const usedBucketName = bucketName
+        || `${preName}${Constants.BUCKET_NAME_TEST}${Utils.randomString()}`.toLocaleLowerCase();
+    this.addToSaved('bucketName', usedBucketName);
+    this.addCommandParameter({ bucket: usedBucketName });
     await S3.createBucket(this.getCommandParameters());
     if (withVersioning === 'with') {
-        this.addCommandParameter({ versioningConfiguration: `Status=Enabled` });
+        this.addCommandParameter({ versioningConfiguration: 'Status=Enabled' });
         await S3.putBucketVersioning(this.getCommandParameters());
     }
     if (withObjectLock === 'with') {
         this.resetCommand();
-        this.addCommandParameter({ bucket: bucketName });
-        this.addCommandParameter({ 
+        this.addCommandParameter({ bucket: usedBucketName });
+        this.addCommandParameter({
             objectLockConfiguration: '{ ' +
-            '"ObjectLockEnabled": "Enabled",' +
-            '"Rule": {' +
-            '"DefaultRetention":' +
-            `{ "Mode": "${retentionMode}", "Days": 50 }}}`
+                '"ObjectLockEnabled": "Enabled",' +
+                '"Rule": {' +
+                '"DefaultRetention":' +
+                `{ "Mode": "${retentionMode}", "Days": 50 }}}`,
         });
         await S3.putObjectLockConfiguration(this.getCommandParameters());
     }
 });
 
 Given('an object {string} that {string}',
-    async function (objectName, objectExists) {
+    async function (this: Zenko, objectName: string, objectExists: string) {
         this.resetCommand();
         if (objectExists === 'exists') {
-            this.saved.objectName = objectName || Utils.randomString();
-            this.addCommandParameter({ key: this.saved.objectName });
-            this.addCommandParameter({ bucket: this.saved.bucketName });
-            this.saved.versionId = extractPropertyFromResults(
+            this.addToSaved('objectName', objectName || Utils.randomString());
+            this.addCommandParameter({ key: this.getSaved<string>('objectName') });
+            this.addCommandParameter({ bucket: this.getSaved<string>('bucketName') });
+            this.addToSaved('versionId', extractPropertyFromResults(
                 await S3.putObject(this.getCommandParameters()), 'VersionId'
-            );
+            ));
         }
     });
