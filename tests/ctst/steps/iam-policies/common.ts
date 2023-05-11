366ff09dafd7954262aca0d98aaa21bcbff00f9c
Will Toozs <william.toozshobson@scality.com>
2023-05-11 17:44:56 +0200

    ZENKO-4524: lint common files

--- tests/ctst/steps/iam-policies/common.ts
+++ tests/ctst/steps/iam-policies/common.ts
@@ -1,59 +1,59 @@
 import { When, Then } from '@cucumber/cucumber';
 import { strict as assert } from 'assert';
-import { EntityType } from "../../world/Zenko";
+import Zenko, { ApiResult, EntityType, UserCredentials } from '../../world/Zenko';
 import { CacheHelper, ClientOptions, S3, VaultAuth } from 'cli-testing';
 import { s3FunctionExtraParams } from '../../common/utils';
 
-When('the user tries to perform {string} on the bucket', async function (action: string) {
-    
-    let userCredentials;
-    if ([EntityType.IAM_USER, EntityType.ACCOUNT].includes(this.saved.type)) {
+When('the user tries to perform {string} on the bucket', async function (this: Zenko, action: string) {
+    let userCredentials: UserCredentials;
+    if ([EntityType.IAM_USER, EntityType.ACCOUNT].includes(this.getSaved<EntityType>('type'))) {
         userCredentials = this.parameters.IAMSession;
         this.resumeRootOrIamUser();
     } else {
-        userCredentials = this.parameters.AssumedSession;
+        userCredentials = this.parameters.AssumedSession!;
         this.resumeAssumedRole();
     }
     switch (action) {
-        case 'MetadataSearch': {
-            this.result = await this.metadataSearchResponseCode(userCredentials, this.saved.bucketName);
-            break;
+    case 'MetadataSearch': {
+        this.setResult(await this.metadataSearchResponseCode(userCredentials, this.getSaved<string>('bucketName')));
+        break;
+    }
+    case 'PutObjectVersion': {
+        this.setResult(await this.putObjectVersionResponseCode(userCredentials,
+            this.getSaved<string>('bucketName'), this.getSaved<string>('objectName')));
+        break;
+    }
+    default: {
+        this.resetCommand();
+        this.addToSaved('ifS3Standard', true);
+        this.addCommandParameter({ bucket: this.getSaved<string>('bucketName') });
+        if (this.getSaved<string>('objectName')) {
+            this.addCommandParameter({ key: this.getSaved<string>('objectName') });
         }
-        case 'PutObjectVersion': {
-            this.result = await this.putObjectVersionResponseCode(userCredentials, this.saved.bucketName, this.saved.objectName);
-            break;
+        if (this.getSaved<string>('versionId')) {
+            this.addCommandParameter({ versionId: this.getSaved<string>('versionId') });
         }
-        default: {
-            this.resetCommand();
-            this.saved.ifS3Standard = true;
-            this.addCommandParameter({bucket: this.saved.bucketName});
-            if (this.saved.objectName) {
-                this.addCommandParameter({key: this.saved.objectName});
+        const usedAction = action.charAt(0).toLowerCase() + action.slice(1);
+        const actionCall = S3[usedAction];
+        if (actionCall) {
+            if (usedAction in s3FunctionExtraParams) {
+                this.addCommandParameter(s3FunctionExtraParams[usedAction]);
             }
-            if (this.saved.versionId) {
-                this.addCommandParameter({versionId: this.saved.versionId});
-            }
-            action = action.charAt(0).toLowerCase() + action.slice(1);
-            const actionCall = (S3 as {[key: string]: Function})[action];
-            if (actionCall) {
-                if (action in s3FunctionExtraParams) {
-                    this.addCommandParameter(s3FunctionExtraParams[action]);
-                }
-                this.result = await actionCall(this.getCommandParameters());
-            } else {
-                throw new Error(`Action ${action} is not supported yet`);
-            }
-            break;
+            this.setResult(await actionCall(this.getCommandParameters()));
+        } else {
+            throw new Error(`Action ${usedAction} is not supported yet`);
         }
+        break;
+    }
     }
 });
 
-When('the user tries to perform vault auth {string}', async function (action: string) {
+When('the user tries to perform vault auth {string}', async function (this: Zenko, action: string) {
     let userCredentials;
-    if ([EntityType.IAM_USER, EntityType.ACCOUNT].includes(this.saved.type)) {
+    if ([EntityType.IAM_USER, EntityType.ACCOUNT].includes(this.getSaved<EntityType>('type'))) {
         userCredentials = this.parameters.IAMSession;
     } else {
-        userCredentials = this.parameters.AssumedSession;
+        userCredentials = this.parameters.AssumedSession!;
     }
 
     if (!this.parameters.VaultAuthHost) {
@@ -66,36 +66,36 @@ When('the user tries to perform vault auth {string}', async function (action: st
         SessionToken: userCredentials.SessionToken,
         ip: this.parameters.VaultAuthHost,
         ssl: CacheHelper.parameters ? CacheHelper.parameters.ssl as boolean : undefined,
-    }
+    };
 
     switch (action) {
-        case 'GetAccountInfo':
-            this.result = await VaultAuth.getAccounts(null, null, null, {}, vaultAuthClientOptions);
-            break;
-        default:
-            throw new Error(`Action ${action} is not supported`);
+    case 'GetAccountInfo':
+        this.setResult(await VaultAuth.getAccounts(null, null, null, {}, vaultAuthClientOptions) as ApiResult);
+        break;
+    default:
+        throw new Error(`Action ${action} is not supported`);
     }
 });
 
-Then('the user should be able to perform successfully the {string} action', function (action : string) {
+Then('the user should be able to perform successfully the {string} action', function (this: Zenko, action: string) {
     this.cleanupEntity();
     switch (action) {
-        case 'MetadataSearch': {
-            assert.strictEqual(this.result?.statusCode, 200);
-            break;
-        }
-        case 'GetAccountInfo': {
-            assert.strictEqual(this.result instanceof Error, false);
-            break;
-        }
-        default: {
-            assert.strictEqual(this.result?.err, null);
-        }
+    case 'MetadataSearch': {
+        assert.strictEqual(this.getResult().statusCode, 200);
+        break;
+    }
+    case 'GetAccountInfo': {
+        assert.strictEqual(this.getResult() instanceof Error, false);
+        break;
+    }
+    default: {
+        assert.strictEqual(this.getResult().err, null);
+    }
 
     }
 });
 
-Then('the user should receive {string} error', function (error : string) {
+Then('the user should receive {string} error', function (this: Zenko, error: string) {
     this.cleanupEntity();
-    assert.strictEqual(this.result.err.includes(error), true);
+    assert.strictEqual(this.getResult().err!.includes(error), true);
 });
