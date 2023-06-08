521414f3f61d7d86131cf0006598f980a530ebcf
Will Toozs <william.toozshobson@scality.com>
2023-06-08 10:44:58 +0200

    ZENKO-4486: update object lock bucket setup test logic

--- tests/ctst/common/common.ts
+++ tests/ctst/common/common.ts
@@ -28,12 +28,16 @@ Given('an existing bucket {string} {string} versioning, {string} ObjectLock {str
         || `${preName}${Constants.BUCKET_NAME_TEST}${Utils.randomString()}`.toLocaleLowerCase();
     this.addToSaved('bucketName', usedBucketName);
     this.addCommandParameter({ bucket: usedBucketName });
+    if (withObjectLock === 'with') {
+        // Empty strings are used to pass parameters that are used as a flag and do not require a value
+        this.addCommandParameter({ objectLockEnabledForBucket: ' ' });
+    }
     await S3.createBucket(this.getCommandParameters());
     if (withVersioning === 'with') {
         this.addCommandParameter({ versioningConfiguration: 'Status=Enabled' });
         await S3.putBucketVersioning(this.getCommandParameters());
     }
-    if (withObjectLock === 'with') {
+    if (retentionMode === 'GOVERNANCE' || retentionMode === 'COMPLIANCE') {
         this.resetCommand();
         this.addCommandParameter({ bucket: usedBucketName });
         this.addCommandParameter({
