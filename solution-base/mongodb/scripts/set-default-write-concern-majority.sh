cfbfe3870b6d63cbf047f28b1569cbdf0c5cbdc0
williamlardier <william.lardier@scality.com>
2023-07-05 16:14:30 +0200

    adapt default write concern majority

--- solution-base/mongodb/scripts/set-default-write-concern-majority.sh
+++ solution-base/mongodb/scripts/set-default-write-concern-majority.sh
@@ -2,13 +2,20 @@
 
 set_default_majority() {
     mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}"  -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
-conf = rs.config()
+var version = db.version().split(".");
+var major = parseInt(version[0]);
+var minor = parseInt(version[1]);
 
-defaultWriteConcern = conf.settings.getLastErrorDefaults
-
-if (!defaultWriteConcern || defaultWriteConcern.w !== 'majority') {
-    defaultWriteConcern.w = 'majority'
-    rs.reconfig(conf)
+if (major > 4 || (major == 4 && minor >= 4)) {
+    var writeConcern = { w: "majority" }
+    db.adminCommand( { setDefaultRWConcern: 1, defaultWriteConcern: writeConcern } )
+} else {
+    conf = rs.config()
+    defaultWriteConcern = conf.settings.getLastErrorDefaults
+    if (!defaultWriteConcern || defaultWriteConcern.w !== 'majority') {
+        defaultWriteConcern.w = 'majority'
+        rs.reconfig(conf)
+    }
 }
 EOF
 }
