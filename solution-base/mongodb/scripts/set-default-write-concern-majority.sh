9b113cf2d8a1f427434d7afbc106c7064b871c1e
williamlardier <william.lardier@scality.com>
2023-07-05 16:50:22 +0200

    adapt set default write permission

--- solution-base/mongodb/scripts/set-default-write-concern-majority.sh
+++ solution-base/mongodb/scripts/set-default-write-concern-majority.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-set_default_majority() {
-    mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}"  -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
-var version = db.version().split(".");
-var major = parseInt(version[0]);
-var minor = parseInt(version[1]);
+get_mongodb_version() {
+    mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}" -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
+db.version().split(".")
+EOF
+}
 
-if (major > 4 || (major == 4 && minor >= 4)) {
-    var writeConcern = { w: "majority" }
-    db.adminCommand( { setDefaultRWConcern: 1, defaultWriteConcern: writeConcern } )
-} else {
-    conf = rs.config()
-    defaultWriteConcern = conf.settings.getLastErrorDefaults
-    if (!defaultWriteConcern || defaultWriteConcern.w !== 'majority') {
-        defaultWriteConcern.w = 'majority'
-        rs.reconfig(conf)
-    }
+set_default_majority_if_mongos() {
+    mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}" -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
+var writeConcern = { w: "majority" }
+db.adminCommand( { setDefaultRWConcern: 1, defaultWriteConcern: writeConcern } )
+EOF
+}
+
+set_default_majority_if_not_configsvr() {
+    mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}" -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
+var conf = rs.config();
+var defaultWriteConcern = conf.settings.getLastErrorDefaults;
+if (!defaultWriteConcern || defaultWriteConcern.w !== 'majority') {
+    defaultWriteConcern.w = 'majority';
+    rs.reconfig(conf);
 }
 EOF
 }
 
-if [ "${MONGODB_SHARDING_MODE:-}" != "configsvr" ]; then
-    set_default_majority
+version_array=$(get_mongodb_version)
+major=$(echo $version_array | cut -d. -f1)
+minor=$(echo $version_array | cut -d. -f2)
+
+# Handling of MongoDB 4.4+
+if [ "${MONGODB_SHARDING_MODE:-}" == "mongos" ] && [ "$major" -ge 4 ] && [ "$minor" -ge 4 ]; then
+    set_default_majority_if_mongos
+fi
+
+# Handling of MongoDB <4.4
+if [ "${MONGODB_SHARDING_MODE:-}" != "configsvr" ] && ([ "$major" -lt 4 ] || ([ "$major" -eq 4 ] && [ "$minor" -lt 4 ])); then
+    set_default_majority_if_not_configsvr
 fi
