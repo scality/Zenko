ad2c05ab36a5c057f9ac3be06e2547a45b31ac47
williamlardier <william.lardier@scality.com>
2023-09-26 11:48:16 +0200

    ZENKO-4658: create new variables for mongodb resources

--- solution-base/build.sh
+++ solution-base/build.sh
@@ -65,6 +65,11 @@ MONGODB_SHARDED_EXPORTER_IMAGE_TAG=$(yq eval ".mongodb-sharded-exporter.tag" $SO
 MONGODB_SHARDED_SHELL_IMAGE_NAME="bitnami-shell"
 MONGODB_SHARDED_SHELL_IMAGE_TAG=$(yq eval ".mongodb-shell.tag" $SOLUTION_BASE_DIR/deps.yaml)
 MONGODB_STORAGE_CLASS="MONGODB_STORAGE_CLASS"
+MONGODB_MONGOS_RAM_LIMIT="MONGODB_MONGOS_RAM_LIMIT"
+MONGODB_WIREDTIGER_CACHE_SIZE_GB="MONGODB_WIREDTIGER_CACHE_SIZE_GB"
+MONGODB_SHARDSERVER_RAM_REQUEST="MONGODB_SHARDSERVER_RAM_REQUEST"
+MONGODB_MONGOS_RAM_REQUEST="MONGODB_MONGOS_RAM_REQUEST"
+MONGODB_SHARDSERVER_RAM_SAFE_LIMIT="MONGODB_SHARDSERVER_RAM_SAFE_LIMIT"
 
 function flatten_source_images()
 {
@@ -194,7 +199,11 @@ function render_mongodb_sharded_yamls()
         --set 'configsvr.persistence.selector.matchLabels.app\.kubernetes\.io/name=mongodb-sharded-config' \
         --set 'configsvr.persistence.selector.matchLabels.app\.kubernetes\.io/part-of=zenko' \
         --set "configsvr.mongodbExtraFlags={--setParameter rollbackTimeLimitSecs=259200}" \
-        --set "shardsvr.dataNode.mongodbExtraFlags={--setParameter rollbackTimeLimitSecs=259200}" \
+        --set mongos.resources.requests.memory=${MONGODB_MONGOS_RAM_REQUEST} \
+        --set mongos.resources.limits.memory=${MONGODB_MONGOS_RAM_LIMIT} \
+        --set "shardsvr.dataNode.mongodbExtraFlags={--wiredTigerCacheSizeGB=${MONGODB_WIREDTIGER_CACHE_SIZE_GB}}" \
+        --set shardsvr.dataNode.resources.limits.memory=${MONGODB_SHARDSERVER_RAM_SAFE_LIMIT} \
+        --set shardsvr.dataNode.resources.requests.memory=${MONGODB_SHARDSERVER_RAM_REQUEST} \
         --set existingSecret=${MONGODB_NAME}-db-creds >> ${OUTPUT_PATH}
 }
 
