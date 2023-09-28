698cf8c7033d67ca22396ade6036940c13354e49
williamlardier <william.lardier@scality.com>
2023-09-28 10:30:40 +0200

    ZENKO-4616: support latest mongodb init scripts for shardsvr

---
+++ solution-base/mongodb/scripts/set-default-write-concern-majority-sharded.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set_default_majority_if_mongos() {
+    mongo --host "mongodb://127.0.0.1/?replicaSet=${MONGODB_REPLICA_SET_NAME}" -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
+var writeConcern = { w: "majority" }
+db.adminCommand( { setDefaultRWConcern: 1, defaultWriteConcern: writeConcern } )
+EOF
+}
+
+# Set default write concern to "majority" if the MongoDB mode is "mongos"
+if [ "${MONGODB_SHARDING_MODE:-}" == "mongos" ]; then
+    set_default_majority_if_mongos
+fi
