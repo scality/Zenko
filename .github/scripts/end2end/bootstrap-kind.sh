3c5abb11f3695f2cfbedd253fec555a3fcb73d08
Francois Ferrand <francois.ferrand@scality.com>
2023-10-02 11:43:32 +0200

    Bump k8s 1.28

--- .github/scripts/end2end/bootstrap-kind.sh
+++ .github/scripts/end2end/bootstrap-kind.sh
@@ -2,7 +2,7 @@
 
 set -exu
 
-NODE_IMAGE=${1:-kindest/node:v1.27.3@sha256:3966ac761ae0136263ffdb6cfd4db23ef8a83cba8a463690e98317add2c9ba72}
+NODE_IMAGE=${1:-kindest/node:v1.28.0@sha256:b7a4cad12c197af3ba43202d3efe03246b3f0793f162afb40a33c923952d5b31}
 VOLUME_ROOT=${2:-/artifacts}
 WORKER_NODE_COUNT=${3:-0}
 CLUSTER_NAME=${CLUSTER_NAME:-kind}
