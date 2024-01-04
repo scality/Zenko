925cc7666a3353a76021eb4b6fed1078547a6aed
williamlardier <william.lardier@scality.com>
2024-01-04 16:39:13 +0100

    wip

--- .github/scripts/end2end/run-e2e-ctst.sh
+++ .github/scripts/end2end/run-e2e-ctst.sh
@@ -154,4 +154,4 @@ kubectl run $POD_NAME \
       }
     ]
   }
-}' -- ./run "$COMMAND" $WORLD_PARAMETERS "--parallel $PARALLEL_RUNS --fail-fast --retry $RETRIES --retry-tag-filter @Flaky"
+}' -- ./run "$COMMAND" $WORLD_PARAMETERS "--parallel $PARALLEL_RUNS --retry $RETRIES --retry-tag-filter @Flaky"
