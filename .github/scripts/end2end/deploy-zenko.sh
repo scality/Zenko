99f1da2b163e0762ca4b7397171fc1e3c222d89c
williamlardier <william.lardier@scality.com>
2024-02-12 15:19:35 +0100

    wip

--- .github/scripts/end2end/deploy-zenko.sh
+++ .github/scripts/end2end/deploy-zenko.sh
@@ -129,7 +129,7 @@ done
 
 # auto describe any pod that is still failing at this point
 # include a describe and logs from this pod
-kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\n"}{end}' | while read ns pod; do
-    kubectl describe pod -n $ns $pod
-    kubectl logs -n $ns $pod
-done
+# kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\n"}{end}' | while read ns pod; do
+#     kubectl describe pod -n $ns $pod
+#     kubectl logs -n $ns $pod
+# done
