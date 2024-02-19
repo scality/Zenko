28f64cc6c26927601890974e78d1b638ee614f58
Maha Benzekri <maha.benzekri@scality.com>
2024-02-19 20:37:29 +0100

    ZENKO-4730: switching from mongo to mongosh

--- .github/scripts/end2end/deploy-zenko.sh
+++ .github/scripts/end2end/deploy-zenko.sh
@@ -141,7 +141,7 @@ env $(dependencies_env) envsubst < ${ZENKOVERSION_PATH} | kubectl -n ${NAMESPACE
 env $(dependencies_env) envsubst < ${ZENKO_CR_PATH} | kubectl -n ${NAMESPACE} apply -f -
 
 k_cmd="kubectl -n ${NAMESPACE} get zenko/${ZENKO_NAME}"
-for i in $(seq 1 120); do
+for i in $(seq 1 200); do
     conditions=$($k_cmd -o "jsonpath={.status.conditions}")
     if kubectl wait --for condition=Available --timeout 5s --namespace ${NAMESPACE} zenko/${ZENKO_NAME}; then
         break;
@@ -149,3 +149,9 @@ for i in $(seq 1 120); do
     # Debug log to ease understanding of failures in the CI
     kubectl get pods -A
 done
+# auto describe any pod that is still failing at this point
+# include a describe and logs from this pod
+# kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\n"}{end}' | while read ns pod; do
+#     kubectl describe pod -n $ns $pod
+#     kubectl logs -n $ns $pod
+# done
