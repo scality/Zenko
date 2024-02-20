9af6bb0492f4062f29bf91898456641f82f64a4f
williamlardier <william.lardier@scality.com>
2024-02-20 16:26:00 +0100

    wip

--- tests/ctst/steps/bucket-policies/common.ts
+++ tests/ctst/steps/bucket-policies/common.ts
@@ -246,10 +246,9 @@ Given('a condition for the bucket policy with {string} {string} {string} expecti
 
 Given('a retention date set to {string} days', function (this: Zenko, retentionDays: string) {
     const currentDate = new Date();
-    // date + number of days provided as iso string
     const targetDate = new Date(currentDate.getTime() + (parseInt(retentionDays, 10) * 24 * 60 * 60 * 1000));
     const retentionDate = targetDate.toISOString();
-    this.addToSaved('retentionDate', `Mode=GOVERNANCE,RetainUntilDate=${retentionDate}`);
+    this.addToSaved('retention', `Mode=GOVERNANCE,RetainUntilDate=${retentionDate}`);
 });
 
 Given('an {string} S3 Bucket Policy that {string} with {string} effect for the current API', async function (
