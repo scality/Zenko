403383cc7bce457004071038124aeb19203d0981
KillianG <killian.gardahaut@scality.com>
2024-03-11 16:36:37 +0100

    Issue: {Jira_ticket}

--- .github/scripts/end2end/configure-e2e.sh
+++ .github/scripts/end2end/configure-e2e.sh
@@ -47,6 +47,26 @@ roleRef:
   apiGroup: rbac.authorization.k8s.io
 EOF
 
+KAFKA_REGISTRY_NAME=$(yq eval ".kafka.sourceRegistry" ../../../solution/deps.yaml)
+KAFKA_IMAGE_NAME=$(yq eval ".kafka.image" ../../../solution/deps.yaml)
+KAFKA_IMAGE_TAG=$(yq eval ".kafka.tag" ../../../solution/deps.yaml)
+KAFKA_IMAGE=$KAFKA_REGISTRY_NAME/$KAFKA_IMAGE_NAME:$KAFKA_IMAGE_TAG
+KAFKA_HOST_PORT=$(kubectl get secret -l app.kubernetes.io/name=backbeat-config,app.kubernetes.io/instance=end2end \
+    -o jsonpath='{.items[0].data.config\.json}' | base64 -di | jq .kafka.hosts)
+KAFKA_HOST_PORT=${KAFKA_HOST_PORT:1:-1}
+
+# Creating bucket notification topic in kafka
+kubectl run kafka-topics \
+    --image=$KAFKA_IMAGE \
+    --pod-running-timeout=5m \
+    --rm \
+    --restart=Never \
+    --attach=True \
+    --command -- bash -c \
+    "kafka-topics.sh --create --topic $NOTIF_DEST_TOPIC --bootstrap-server $KAFKA_HOST_PORT ; \
+        kafka-topics.sh --create --topic $NOTIF_ALT_DEST_TOPIC --bootstrap-server $KAFKA_HOST_PORT ; \
+        kafka-topics.sh --create --topic $UUID.backbeat-replication-group-awsbackendmismatch --bootstrap-server $KAFKA_HOST_PORT"
+
 kubectl run ${POD_NAME} \
   --image ${E2E_IMAGE} \
   --rm \
@@ -84,20 +104,3 @@ sleep 10
 
 kubectl wait --for condition=DeploymentFailure=false --timeout 10m -n ${NAMESPACE} zenko/${ZENKO_NAME}
 kubectl wait --for condition=DeploymentInProgress=false --timeout 10m -n ${NAMESPACE} zenko/${ZENKO_NAME}
-
-KAFKA_REGISTRY_NAME=$(yq eval ".kafka.sourceRegistry" ../../../solution/deps.yaml)
-KAFKA_IMAGE_NAME=$(yq eval ".kafka.image" ../../../solution/deps.yaml)
-KAFKA_IMAGE_TAG=$(yq eval ".kafka.tag" ../../../solution/deps.yaml)
-KAFKA_IMAGE=$KAFKA_REGISTRY_NAME/$KAFKA_IMAGE_NAME:$KAFKA_IMAGE_TAG
-
-# Creating bucket notification topic in kafka
-kubectl run kafka-topics \
-    --image=$KAFKA_IMAGE \
-    --pod-running-timeout=5m \
-    --rm \
-    --restart=Never \
-    --attach=True \
-    --command -- bash -c \
-    "kafka-topics.sh --create --topic $NOTIF_DEST_TOPIC --bootstrap-server $KAFKA_HOST_PORT ; \
-        kafka-topics.sh --create --topic $NOTIF_ALT_DEST_TOPIC --bootstrap-server $KAFKA_HOST_PORT ; \
-        kafka-topics.sh --create --topic $UUID.backbeat-replication-group-awsbackendmismatch --bootstrap-server $KAFKA_HOST_PORT"
