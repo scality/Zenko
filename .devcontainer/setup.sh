62dafb5f096c681f8ecb0dd57ea921013a410b24
KillianG <killian.gardahaut@scality.com>
2024-04-12 08:51:11 +0000

    Move setup out of startup

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,14 @@
+CURRENT_DIR=$(pwd)
+
+cd .github/scripts/end2end/
+
+bash ./bootstrap-kind.sh ${KIND_NODE_IMAGE} ${VOLUME_ROOT} ${WORKER_COUNT}
+bash ./create-pull-image-secret.sh
+bash ./install-kind-dependencies.sh
+bash ./patch-coredns.sh
+bash ./deploy-shell-ui.sh
+bash ./keycloak-helper.sh setup-realm default
+
+cd $CURRENT_DIR
+
+bash .devcontainer/startup-zkop.sh
