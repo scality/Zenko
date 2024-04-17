493ca913d896ca98451108d12776dfcba832ebf2
KillianG <killian.gardahaut@scality.com>
2024-04-17 15:25:05 +0000

    Get env from existing file instead of devcontainer
    Issue: ZENKO-4799

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+env_variables=$(yq eval '.env | to_entries | .[] | .key + "=" + .value' .github/workflows/end2end.yaml | sed 's/\${{[^}]*}}//g') && export $env_variables
 export GIT_ACCESS_TOKEN=${GITHUB_TOKEN}
+export E2E_IMAGE_TAG=latest
 
 echo $REGISTRY_PASSWORD | docker login registry.scality.com -u $REGISTRY_LOGIN --password-stdin
-(
     array_length=`yq ".runs.steps | length - 1" .github/actions/deploy/action.yaml`
     for i in $(seq 0 $array_length); do
         step=`yq ".runs.steps[$i]" .github/actions/deploy/action.yaml`
@@ -11,20 +13,21 @@ echo $REGISTRY_PASSWORD | docker login registry.scality.com -u $REGISTRY_LOGIN -
 
         # We don't want to run `run-e2e-test.sh` because it is used for linting here, user will run it manually if needed after deployment
         # We can't run `configure-e2e.sh` here because it needs an image that is not yet built and sent to kind, will be run after
-        if [[ "$run_command" != "null" && "$run_command" != *"configure-e2e.sh"* && "$run_command" != *"run-e2e-test.sh"* ]]
-        then
-            if [ "$working_dir" != "null" ]
+        (
+            if [[ "$run_command" != "null" && "$run_command" != *"configure-e2e.sh"* && "$run_command" != *"run-e2e-test.sh"* ]]
             then
-                echo "Changing working dir: $working_dir"
-                #cd $working_dir
+                if [ "$working_dir" != "null" ]
+                then
+                    echo "Changing working dir: $working_dir"
+                    cd $working_dir
+                fi
+                echo "Run command: $run_command"
+                while IFS= read -r line; do
+                    eval $line
+                done <<< "$run_command";
             fi
-            echo "Run command: $run_command"
-            while IFS= read -r line; do
-                eval $line
-            done <<< "$run_command";
-        fi
+        )
     done
-)
 
 (
     cd tests/zenko_tests
@@ -34,13 +37,12 @@ echo $REGISTRY_PASSWORD | docker login registry.scality.com -u $REGISTRY_LOGIN -
     yq -i 'del(.locations[] | select(.locationType == "location-scality-ring-s3-v1"))' e2e-config.yaml
     fi
     docker build -t $E2E_IMAGE_NAME:$E2E_IMAGE_TAG .
+    kind load docker-image  ${E2E_IMAGE_NAME}:${E2E_IMAGE_TAG}
+    docker rmi ${E2E_IMAGE_NAME}:${E2E_IMAGE_TAG}
 )
 
 (
     cd .github/scripts/end2end
-
-    kind load docker-image  ${E2E_IMAGE_NAME}:${E2E_IMAGE_TAG}
-    docker rmi ${E2E_IMAGE_NAME}:${E2E_IMAGE_TAG}
 
     bash configure-e2e-ctst.sh
 )
@@ -51,4 +53,3 @@ CTST_TAG=$(sed 's/.*"cli-testing": ".*#\(.*\)".*/\1/;t;d' ./tests/ctst/package.j
 SORBET_TAG=$(yq eval '.sorbet.tag' solution/deps.yaml)
 docker build --build-arg CTST_TAG=$CTST_TAG --build-arg SORBET_TAG=$SORBET_TAG -t $E2E_CTST_IMAGE_NAME:$E2E_IMAGE_TAG ./tests/ctst
 kind load docker-image  ${E2E_CTST_IMAGE_NAME}:${E2E_IMAGE_TAG}
-docker rmi ${E2E_CTST_IMAGE_NAME}:${E2E_IMAGE_TAG}
