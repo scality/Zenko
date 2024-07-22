00c3499a54b0a23d5bc9fa7795f8ec435258a809
KillianG <Killian.gardahaut@scality.com>
2024-07-22 21:45:03 +0200

    Refactor PRA scenario to remove unused kubeconfig paths

--- tests/ctst/steps/pra.ts
+++ tests/ctst/steps/pra.ts
@@ -33,8 +33,6 @@ Given('a DR installed', { timeout: 130000 }, async function (this: Zenko) {
         sourceS3UserSecretName: 'end2end-management-vault-admin-creds.v1',
         sourceS3Endpoint: 'http://s3.zenko.local',
         sinkS3Endpoint: 'http://s3.dr.zenko.local',
-        sinkKubeconfigPath: '/etc/kubernetes/admin.conf',
-        sourceKubeconfigPath: '/etc/kubernetes/admin.conf',
     });
     return;
 });
