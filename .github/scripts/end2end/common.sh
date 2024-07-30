f568fe29362cf53e36ac7a2a3d9c6f0277979498
KillianG <Killian.gardahaut@scality.com>
2024-07-30 14:40:27 +0200

    wtf

--- .github/scripts/end2end/common.sh
+++ .github/scripts/end2end/common.sh
@@ -1,6 +1,5 @@
 get_token() {
     HOST=${1:-keycloak.zenko.local}
-    echo "Getting token for ${HOST}"
     curl -k -H "Host: ${HOST}" \
         -d "client_id=${OIDC_CLIENT_ID}" \
         -d "username=${OIDC_USERNAME}" \
