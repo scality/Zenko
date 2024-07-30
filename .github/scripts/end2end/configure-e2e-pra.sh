c48246b428efff40faeccd62b3186cd3fb2bedba
KillianG <Killian.gardahaut@scality.com>
2024-07-30 17:42:19 +0200

    chore: Update configure-e2e-pra.sh to use correct host for management API

--- .github/scripts/end2end/configure-e2e-pra.sh
+++ .github/scripts/end2end/configure-e2e-pra.sh
@@ -10,7 +10,8 @@ TOKEN=$(get_token)
 LOCATION_PARAMS='{"name":"e2e-cold","locationType":"location-dmf-v1","details":{"endpoint":"ws://mock-sorbet:5001/session","username":"user1","password":"pass1","repoId":["233aead6-1d7b-4647-a7cf-0d3280b5d1d7","81e78de8-df11-4acd-8ad1-577ff05a68db"],"nsId":"65f9fd61-42fe-4a68-9ac0-6ba25311cc85"}}'
 
 curl -k -X POST \
+-H "Host: management.dr.zenko.local" \
 -H "X-Authentication-Token: $TOKEN" \
 -H "Content-Type: application/json" \
 -d "$LOCATION_PARAMS" \
-"https://management.dr.zenko.local/api/v1/config/$INSTANCE_ID/location"
+"https://localhost/api/v1/config/$INSTANCE_ID/location"
