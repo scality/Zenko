f8bf3b5e7e89bd14e1ae249f659d2b5e5942afae
KillianG <Killian.gardahaut@scality.com>
2024-07-30 11:39:20 +0200

    Update end-to-end configuration for DR site

---
+++ .github/scripts/end2end/configure-e2e-pra.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -exu
+
+. "$(dirname $0)/common.sh"
+
+ZENKO_NAME=${1:-end2end}
+E2E_IMAGE=${2:-ghcr.io/scality/zenko/zenko-e2e:latest}
+NAMESPACE=${3:-default}
+
+SERVICE_ACCOUNT="${ZENKO_NAME}-config"
+POD_NAME="${ZENKO_NAME}-config"
+MANAGEMENT_ENDPOINT="http://${ZENKO_NAME}-management-orbit-api:5001"
+VAULT_ENDPOINT="http://${ZENKO_NAME}-connector-vault-sts-api"
+UUID=$(kubectl get zenko ${ZENKO_NAME} --namespace ${NAMESPACE} -o jsonpath='{.status.instanceID}')
+TOKEN=$(get_token)
+
+
+kubectl run ${POD_NAME} \
+  --image ${E2E_IMAGE} \
+  --rm \
+  --attach=True \
+  --restart=Never \
+  --pod-running-timeout=5m \
+  --namespace=${NAMESPACE} \
+  --image-pull-policy=IfNotPresent \
+  --overrides="{ \"spec\": { \"serviceAccount\": \"${SERVICE_ACCOUNT}\" }  }" \
+  --env="TOKEN=${TOKEN}" \
+  --env="UUID=${UUID}" \
+  --env="MANAGEMENT_ENDPOINT=${MANAGEMENT_ENDPOINT}" \
+  --env="VAULT_ENDPOINT=${VAULT_ENDPOINT}" \
+  --env="NAMESPACE=${NAMESPACE}" \
+  --env=VERIFY_CERTIFICATES=false \
+  --env=ENABLE_RING_TESTS=${ENABLE_RING_TESTS} \
+  --env=RING_S3C_ACCESS_KEY=${RING_S3C_ACCESS_KEY} \
+  --env=RING_S3C_SECRET_KEY=${RING_S3C_SECRET_KEY} \
+  --env=RING_S3C_ENDPOINT=${RING_S3C_ENDPOINT} \
+  --env=RING_S3C_BACKEND_SOURCE_LOCATION=${RING_S3C_BACKEND_SOURCE_LOCATION} \
+  --env=RING_S3C_INGESTION_SRC_BUCKET_NAME=${RING_S3C_INGESTION_SRC_BUCKET_NAME} \
+  --env=AWS_ACCESS_KEY=${AWS_ACCESS_KEY} \
+  --env=AWS_SECRET_KEY=${AWS_SECRET_KEY} \
+  --env=AWS_ENDPOINT=${AWS_ENDPOINT} \
+  --env=AWS_FAIL_BUCKET_NAME=${AWS_FAIL_BUCKET_NAME} \
+  --env=AZURE_BACKEND_DESTINATION_LOCATION=${AZURE_BACKEND_DESTINATION_LOCATION} \
+  --env=AZURE_BACKEND_ENDPOINT=${AZURE_BACKEND_ENDPOINT} \
+  --env=AZURE_ACCOUNT_NAME=${AZURE_ACCOUNT_NAME} \
+  --env=AZURE_SECRET_KEY=${AZURE_SECRET_KEY} \
+  --env=AZURE_CRR_BUCKET_NAME=${AZURE_CRR_BUCKET_NAME} \
+  --command -- python3 configuration.py
+
+## wait for updates to trigger zenko upgrades
+sleep 10
+
+kubectl wait --for condition=DeploymentFailure=false --timeout 10m -n ${NAMESPACE} zenko/${ZENKO_NAME}
+kubectl wait --for condition=DeploymentInProgress=false --timeout 10m -n ${NAMESPACE} zenko/${ZENKO_NAME}
