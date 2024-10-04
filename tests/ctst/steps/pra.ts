9fb37de6fdbd9b96a90c0b5207414ac818a39895
KillianG <Killian.gardahaut@scality.com>
2024-10-04 08:04:02 +0200

    Fix failover
    Issue: ZENKO-4903

--- tests/ctst/steps/pra.ts
+++ tests/ctst/steps/pra.ts
@@ -325,6 +325,7 @@ const failoverTimeout = 360000;
 When ('I request the failover state for the DR', { timeout: failoverTimeout + 2000 }, async function (this: Zenko) {
     await this.zenkoDrCtl?.failover({
         sinkZenkoDrNamespace: 'default',
+        sinkZenkoDrInstance: 'end2end-pra-sink',
         wait: true,
         timeout: `${failoverTimeout.toString()}ms`,
     });
@@ -333,7 +334,8 @@ When ('I request the failover state for the DR', { timeout: failoverTimeout + 20
 const failbackTimeout = 360000;
 When ('I resume operations for the DR', { timeout: failbackTimeout + 2000 }, async function (this: Zenko) {
     await this.zenkoDrCtl?.failback({
-        sinkZenkoNamespace: 'default',
+        sinkZenkoDrNamespace: 'default',
+        sinkZenkoDrInstance: 'end2end-pra-sink',
         wait: true,
         timeout: `${failbackTimeout.toString()}ms`,
     });
