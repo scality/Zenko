839ef2868562eb08e7d40972202d9d062e0085a1
williamlardier <william.lardier@scality.com>
2025-01-02 13:36:53 +0100

    test

--- .github/scripts/end2end/generate-kustomization.sh
+++ .github/scripts/end2end/generate-kustomization.sh
@@ -8,8 +8,8 @@ readonly VALID_TOPOLOGIES=(
 )
 
 generate_kustomization() {
-    local node_count=1
-    local shard_count=1
+    local node_count="$1"
+    local shard_count="$2"
     local kustomization_file="kustomization.yaml"
     local base_yaml="mongodb-sharded-${node_count}-node"
 
