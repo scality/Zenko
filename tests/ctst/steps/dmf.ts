422268f6d20dbf73ada278201302b4fa9cab4b0b
Kerkesni <walid.elansari@scality.com>
2025-02-27 11:19:35 +0100

    increase timeout when waiting for dmf to clean data
    Issue: ZENKO-4975

--- tests/ctst/steps/dmf.ts
+++ tests/ctst/steps/dmf.ts
@@ -7,17 +7,18 @@ async function cleanDmfVolume() {
     await execShellCommand('rm -rf /cold-data/*');
 }
 
-Then('dmf volume should contain {int} objects', async (objectCount: number) => {
-    let conditionOk = false;
-    while (!conditionOk) {
-        // Getting the number of objects inside the volume used
-        // by the mock dmf to store transitioned objects
-        const outStr = await execShellCommand('find /cold-data -type f | wc -l');
-        // we store two files per object (content and manifest.json)
-        conditionOk = Number(outStr) === objectCount * 2;
-    }
-    assert(conditionOk);
-});
+Then('dmf volume should contain {int} objects',
+    { timeout: 2 * 60 * 1000 }, async (objectCount: number) => {
+        let conditionOk = false;
+        while (!conditionOk) {
+            // Getting the number of objects inside the volume used
+            // by the mock dmf to store transitioned objects
+            const outStr = await execShellCommand('find /cold-data -type f | wc -l');
+            // we store two files per object (content and manifest.json)
+            conditionOk = Number(outStr) === objectCount * 2;
+        }
+        assert(conditionOk);
+    });
 
 Given('a flaky backend that will require {int} retries for {string}',
     function (this: Zenko, retryNumber: number, op: string) {
