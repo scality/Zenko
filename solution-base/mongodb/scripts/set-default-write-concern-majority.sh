1d1ba48c5b4bc45049bf81595b921fe71f017482
Maha Benzekri <maha.benzekri@scality.com>
2025-03-07 17:10:26 +0100

    setting defaultWriteConcern cluster wide
    Issue: ZENKO-4950

--- solution-base/mongodb/scripts/set-default-write-concern-majority.sh
+++ solution-base/mongodb/scripts/set-default-write-concern-majority.sh
@@ -4,15 +4,27 @@ set_default_majority() {
     mongosh "mongodb://127.0.0.1:27017/?replicaSet=${MONGODB_REPLICA_SET_NAME}"  -u 'root' -p "$MONGODB_ROOT_PASSWORD" <<EOF
 conf = rs.config()
 
-defaultWriteConcern = conf.settings.getLastErrorDefaults
+try {
+    let currentDefault = db.adminCommand({ getDefaultRWConcern: 1 })
 
-if (!defaultWriteConcern || defaultWriteConcern.w !== 'majority') {
-    defaultWriteConcern.w = 'majority'
-    rs.reconfig(conf)
+    if (!currentDefault.defaultWriteConcern || currentDefault.defaultWriteConcern.w !== 'majority') {
+        db.adminCommand({
+            setDefaultRWConcern: 1,
+            defaultWriteConcern: { w: 'majority' }
+        })
+        print("Default write concern set to 'majority'")
+    } else {
+        print("Default write concern already set to 'majority'")
+    }
+} catch (e) {
+    print("Error setting default write concern: " + e)
+    quit(1)
 }
 EOF
 }
 
-if [ "${MONGODB_SHARDING_MODE:-}" != "configsvr" ]; then
+if [ "${MONGODB_SHARDING_MODE:-}" == "configsvr" ]; then
     set_default_majority
+else
+    echo "Skipping write concern configuration as this is not a config server."
 fi
