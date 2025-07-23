72d1bf5d84106cd9840aae702b97163381b16b22
DarkIsDude <edouard.comtet@gmail.com>
2025-07-23 06:19:18 +0200

    test to run a dedicated test in the CI instead of all

--- .github/scripts/end2end/run-e2e-ctst.sh
+++ .github/scripts/end2end/run-e2e-ctst.sh
@@ -207,4 +207,4 @@ kubectl run $POD_NAME \
       }
     ]
   }
-}' -- ./run "$COMMAND" $WORLD_PARAMETERS --parallel $PARALLEL_RUNS --retry $RETRIES --retry-tag-filter @Flaky --format junit:$JUNIT_REPORT_PATH "$@"
+}' -- ./run "$COMMAND" $WORLD_PARAMETERS --parallel $PARALLEL_RUNS --retry $RETRIES --retry-tag-filter @Flaky --format junit:$JUNIT_REPORT_PATH "$@" --tags '@Only'
--- tests/ctst/features/put-object.feature
+++ tests/ctst/features/put-object.feature
@@ -2,6 +2,7 @@ Feature: Put Object operations
 
     @2.6.0
     @PreMerge
+    @Only
     Scenario Outline: Handle versionId collision on PutObject
     Given a "Versioned" bucket
     And 100 versions of objects "obj" of size 100 bytes with 10 threads
