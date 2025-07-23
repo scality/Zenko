332ac45c0ed655e73f2d7ebdad62ae73a535b2a8
DarkIsDude <edouard.comtet@gmail.com>
2025-07-23 14:31:54 +0200

    update test to test the number of versions created on the same object
    Issue: ZENKO-5023

--- tests/ctst/steps/put-object.ts
+++ tests/ctst/steps/put-object.ts
@@ -1,52 +1,65 @@
-import { Then } from '@cucumber/cucumber';
+import {Given, Then} from '@cucumber/cucumber';
 import Zenko from 'world/Zenko';
-import { S3 } from 'cli-testing';
-import { ListObjectsV2Output, ListObjectVersionsOutput } from '@aws-sdk/client-s3';
-import { safeJsonParse } from 'common/utils';
+import {S3} from 'cli-testing';
+import {ListObjectsV2Output, ListObjectVersionsOutput} from '@aws-sdk/client-s3';
+import {safeJsonParse} from 'common/utils';
+import assert from 'assert';
 
-Then('no collision should occur on versionId', async function (this: Zenko) {
+Given('{int} versions of objects {string} of size {int} bytes with {int} threads', async function (
+    this: Zenko,
+    numberOfVerionsPerThreads: number,
+    objectName: string,
+    sizeBytes: number,
+    numberOfThreads: number
+) {
+    const bucketName = this.getSaved<string>('bucketName');
+    this.addToSaved('objectName', objectName);
+
+    await Promise.all(Array.from({length: numberOfThreads}, async () => {
+        for (let i = 0; i < numberOfVerionsPerThreads; i++) {
+            const result = await S3.putObject({
+                bucket: bucketName,
+                key: objectName,
+                body: Buffer.alloc(sizeBytes, 'a').toString('utf8'),
+            });
+            assert.ifError(result?.stderr || result?.err);
+        }
+    }));
+});
+
+Then('{int} versions of objects {string} should exist', async function (
+    this: Zenko,
+    expectedNumberOfVersions: number,
+    objectName: string
+) {
     const bucketName = this.getSaved<string>('bucketName');
     const results = await S3.listObjectsV2({
         bucket: bucketName,
         maxItems: '1000',
+        prefix: objectName,
     });
     const res = safeJsonParse<ListObjectsV2Output>(results.stdout);
+    assert.ok(res.ok, `Failed to list objects in bucket ${bucketName}: ${res.error}`);
+    assert.equal(
+        res.result?.Contents,
+        1,
+        `Expected 1 object with prefix ${objectName} in bucket ${bucketName}, found ${res.result?.Contents?.length}`
+    );
 
-    if (!res.ok) {
-        throw new Error(`Failed to list objects in bucket ${bucketName}: ${res.error}`);
-    }
-    
-    const versionsProcessed = new Set<string>();
-    
-    for (const object of res.result?.Contents || []) {
-        if (!object.Key) {
-            throw new Error(`Object in bucket ${bucketName} has no Key: ${JSON.stringify(object)}`);
-        }
+    const object = res.result!.Contents![0];
+    assert.ok(object.Key, `Object in bucket ${bucketName} has no Key: ${JSON.stringify(object)}`);
 
-        const versions = await S3.listObjectVersions({
-            bucket: bucketName,
-            prefix: object.Key,
-        });
-        const versionsRes = safeJsonParse<ListObjectVersionsOutput>(versions.stdout);
-
-        if (!versionsRes.ok) {
-            throw new Error(
-                `Failed to list versions for object ${object.Key} in bucket ${bucketName}: ${versionsRes.error}`
-            );
-        }
-
-        for (const version of versionsRes.result?.Versions || []) {
-            if (!version.VersionId) {
-                throw new Error(`Version in bucket ${bucketName} has no VersionId: ${JSON.stringify(version)}`);
-            }
-
-            if (versionsProcessed.has(version.VersionId)) {
-                throw new Error(
-                    `Duplicate versionId found: ${version.VersionId} for object ${object.Key} in bucket ${bucketName}`
-                );
-            }
-
-            versionsProcessed.add(version.VersionId);
-        }
-    }
+    const versions = await S3.listObjectVersions({
+        bucket: bucketName,
+        prefix: object.Key,
+        maxItems: '1000',
+    });
+    const versionsRes = safeJsonParse<ListObjectVersionsOutput>(versions.stdout);
+    assert.ok(versionsRes.ok, `Failed to list versions in bucket ${bucketName}: ${versionsRes.error}`);
+    assert.ok(versionsRes.result?.Versions, `No versions found in bucket ${bucketName} for object ${object.Key}`);
+    assert.equal(
+        versionsRes.result.Versions.length,
+        expectedNumberOfVersions,
+        `Expected ${expectedNumberOfVersions} versions for object, found ${versionsRes.result.Versions.length}`
+    );
 });
