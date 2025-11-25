f7bd6846a88b6c6324bf55345a7300c87351143c
Maha Benzekri <maha.benzekri@scality.com>
2025-11-25 20:02:23 +0100

    lint fixup

--- tests/zenko_tests/node_tests/backbeat/tests/crr/awsBackend.js
+++ tests/zenko_tests/node_tests/backbeat/tests/crr/awsBackend.js
@@ -671,7 +671,7 @@ describe('Replication with AWS backend', function () {
     ], done));
 
     it.only('should replicate an object with cache control', done => series([
-        next =>{
+        next => {
             // eslint-disable-next-line no-console
             console.log('Putting object with cache control');
             return next();
