4533cfa9c9195daa23a291b1c9a7eabe50556d2c
Sylvain <senechalsylvain.dev@gmail.com>
2026-02-03 17:56:11 +0000

    test codespace

--- tests/zenko_tests/node_tests/s3SDK.js
+++ tests/zenko_tests/node_tests/s3SDK.js
@@ -1,20 +1,29 @@
 const { S3Client } = require('@aws-sdk/client-s3');
 const { IAMClient } = require('@aws-sdk/client-iam');
 const { NodeHttpHandler } = require('@aws-sdk/node-http-handler');
+const { variables, loadZenkoCredentialsFromK8s } = require('../../../setup/variables');
 
 const sharedHttpHandler = new NodeHttpHandler({
     requestTimeout: 0,
     connectionTimeout: 0,
 });
 
+async function zenkoCredentialsProvider() {
+    if (!variables.accessKey) {
+        await loadZenkoCredentialsFromK8s();
+    }
+    return {
+        accessKeyId: variables.accessKey,
+        secretAccessKey: variables.secretKey,
+        sessionToken: variables.sessionToken,
+    };
+}
+
 const scalityS3Client = new S3Client({
-    credentials: {
-        accessKeyId: process.env.ZENKO_ACCESS_KEY,
-        secretAccessKey: process.env.ZENKO_SECRET_KEY,
-        sessionToken: process.env.ZENKO_SESSION_TOKEN,
-    },
+    credentials: zenkoCredentialsProvider,
     tls: false,
     endpoint: process.env.CLOUDSERVER_ENDPOINT,
+    // endpoint: "http://end2end-connector-s3api.default.svc.cluster.local:80",
     region: 'us-east-1',
     forcePathStyle: true,
     // disable node sdk retries and timeout to prevent InvalidPart
@@ -27,11 +36,7 @@ const scalityS3Client = new S3Client({
 });
 
 const scalityIAMClient = new IAMClient({
-    credentials: {
-        accessKeyId: process.env.ZENKO_ACCESS_KEY,
-        secretAccessKey: process.env.ZENKO_SECRET_KEY,
-        sessionToken: process.env.ZENKO_SESSION_TOKEN,
-    },
+    credentials: zenkoCredentialsProvider,
     tls: false,
     endpoint: process.env.VAULT_ENDPOINT,
     region: 'us-east-1',
