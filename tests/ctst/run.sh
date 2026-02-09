f8fe0311fcaa306e2cb9cf9f49fe23b93ea8963e
Sylvain <senechalsylvain.dev@gmail.com>
2026-02-09 17:08:32 +0000

    some more local tests

--- tests/ctst/run.sh
+++ tests/ctst/run.sh
@@ -4,7 +4,22 @@ set -exu
 # Usage: ./run.sh <tag>  (e.g., ./run.sh Yoyoyo or ./run.sh PreMerge)
 
 CUCUMBER_TAG="${1#@}"
-WORLD_PARAMETERS='{"subdomain":"zenko","ssl":false,"port":"80","AccountName":"zenko","AccountAccessKey":"accessKey1","AccountSecretKey":"secretKey1"}'
+# Version is used with a @Before tags (in cli-testing repo) to skip certain tests.
+VERSION=$(cat ../../VERSION | grep -Po 'VERSION="\K[^"]*')
+
+WORLD_PARAMETERS="$(jq -c <<EOF
+{
+  "subdomain": "zenko.local",
+  "ssl": false,
+  "port": "80",
+  "AccountName": "zenko",
+  "AdminAccessKey": "ZNYO52SR80XW1S0VK6IL",
+  "AdminSecretKey": "Hrk=4qCMpRqL2r4LmgUyfVPLj=PlImnms=FKtwJq"
+}
+EOF
+)"
+
+EMPTY_WORLD_PARAMETERS="{}"
 
 # Create pod only if it doesn't exist
 if ! kubectl get pod ctst-end2end &>/dev/null; then
@@ -17,6 +32,12 @@ if ! kubectl get pod ctst-end2end &>/dev/null; then
     kubectl wait --for=condition=Ready pod/ctst-end2end --timeout=5m
 fi
 
+# Grant cluster-admin role to service accounts to ensure the test pod has permissions
+# to run some kubernetes commands (we have a k8s client inside the test creating pods, jobs, etc.)
+kubectl create clusterrolebinding serviceaccounts-cluster-admin \
+  --clusterrole=cluster-admin \
+  --group=system:serviceaccounts 2>/dev/null || true
+
 # Copy local test files so that the changes you want to test are included in the test run
 kubectl exec ctst-end2end -- rm -rf /ctst/features /ctst/common /ctst/steps /ctst/world
 kubectl cp ./features ctst-end2end:/ctst/features
@@ -24,4 +45,4 @@ kubectl cp ./common ctst-end2end:/ctst/common
 kubectl cp ./steps ctst-end2end:/ctst/steps
 kubectl cp ./world ctst-end2end:/ctst/world
 
-kubectl exec -it ctst-end2end -- ./run base $WORLD_PARAMETERS --tags "@$CUCUMBER_TAG"
+kubectl exec -it ctst-end2end -- env TARGET_VERSION=$VERSION ./run base $WORLD_PARAMETERS --tags "@$CUCUMBER_TAG"
