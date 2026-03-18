810af78353d945647681dbca0241a067bbdb4796
Thomas Flament <thomas.flament@scality.com>
2026-03-18 09:06:19 +0100

    deploy-zkop: use local zenko-operator checkout if available
    When ../zenko-operator exists, symlink it instead of cloning from
    GitHub. This avoids creating a nested git repo and allows using a
    local working copy for development. Also clean up the operator
    image after loading it into kind.

--- .github/scripts/end2end/deploy-zkop.sh
+++ .github/scripts/end2end/deploy-zkop.sh
@@ -5,9 +5,15 @@ set -ex
 [ -z "${OPERATOR_IMAGE_TAG}" ] && OPERATOR_IMAGE_TAG="$(yq eval '."zenko-operator".tag' solution/deps.yaml)"
 
 OPERATOR_PATH=./.github/scripts/end2end/operator
-git init $OPERATOR_PATH
-cd $OPERATOR_PATH
-git fetch --depth 1 --no-tags https://git:${GIT_ACCESS_TOKEN}@github.com/scality/zenko-operator.git ${OPERATOR_IMAGE_TAG}
-git checkout FETCH_HEAD
+LOCAL_OPERATOR_PATH=../zenko-operator
+if [ -d "$LOCAL_OPERATOR_PATH" ]; then
+    echo "Using local zenko-operator checkout at $LOCAL_OPERATOR_PATH"
+    ln -sfn "$(readlink -f "$LOCAL_OPERATOR_PATH")" "$OPERATOR_PATH"
+else
+    git init $OPERATOR_PATH
+    git -C $OPERATOR_PATH fetch --depth 1 --no-tags https://git:${GIT_ACCESS_TOKEN}@github.com/scality/zenko-operator.git ${OPERATOR_IMAGE_TAG}
+    git -C $OPERATOR_PATH checkout FETCH_HEAD
+fi
 
+cd $OPERATOR_PATH
 tilt ci
