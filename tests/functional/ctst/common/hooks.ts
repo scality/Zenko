352cfdc31a7112c5144bb4067f5800a2ecf6651b
sylvain senechal <senechalsylvain.dev@gmail.com>
2026-05-21 15:02:18 +0200

    removed useless version tags hook
    Issue: ZENKO-5282

--- tests/functional/ctst/common/hooks.ts
+++ tests/functional/ctst/common/hooks.ts
@@ -23,7 +23,6 @@ import { startDLQConsumer, stopDLQConsumer } from 'steps/utils/kafka';
 
 import 'cli-testing/hooks/KeycloakSetup';
 import 'cli-testing/hooks/Logger';
-import 'cli-testing/hooks/versionTags';
 
 // HTTPS should not cause any error for CTST
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
